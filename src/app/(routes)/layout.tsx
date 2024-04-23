0e9ac6648600290fbda77fffea02fd429e51bfb8
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-04-23 21:19:30 +0200

    Reformatted the root layout

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -2,18 +2,19 @@ import type { Metadata } from "next";
 
 import "~/app/_styles/globals.css";
 import "~/app/_styles/font-faces.css";
+import { FC, PropsWithChildren } from "react";
 
 export const metadata: Metadata = {
   title: "Emil Molt Akademie",
   description: "Wirtschaft verstehen. Sozial handeln.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
+const RootLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="de">
       <body>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
