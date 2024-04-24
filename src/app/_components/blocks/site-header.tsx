e38a47223c4a33ad18e4dc412ceccc4d57323524
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-04-24 14:21:26 +0200

    Added a nav link to the site header

--- src/app/_components/blocks/site-header.tsx
+++ src/app/_components/blocks/site-header.tsx
@@ -6,6 +6,8 @@ import { Container } from "../primitives/container";
 import { SiteLogo } from "../compounds/site-logo";
 import Link from "next/link";
 import { Hamburger } from "../compounds/hamburger";
+import { Label } from "../primitives/typography";
+import { usePathname } from "next/navigation";
 
 export type SiteHeaderProps = ComponentProps<"header"> & {};
 
@@ -19,9 +21,48 @@ export const SiteHeader: FC<SiteHeaderProps> = ({ className, ...restProps }) => 
           <Link href="/">
             <SiteLogo />
           </Link>
-          <Hamburger open={menuOpen} onOpenChange={setMenuOpen} className="md:hiddenx" />
+          <nav className="hidden md:flex items-center gap-10 font-medium text-sm">
+            <NavigationLink href="/" label="Home" exact />
+            {/* <a data-offers-trigger className="group relative cursor-pointer">
+              <InteractionDot />
+              <Typography>Bildungswege</Typography>
+            </a>
+            <a href="/blog" className="group relative">
+              <InteractionDot />
+              <Typography>Blog</Typography>
+            </a>
+            <Button on="light" intent="tertiary-cta">
+              <ButtonText>Bewerben</ButtonText> <ButtonArrow size="sm" />
+            </Button> */}
+          </nav>
+
+          <Hamburger open={menuOpen} onOpenChange={setMenuOpen} className="md:hidden" />
         </div>
       </Container>
     </header>
+  );
+};
+
+type NavigationLinkProps = {
+  href: string;
+  label: string;
+  exact?: boolean;
+};
+
+const NavigationLink: FC<NavigationLinkProps> = ({ href, label, exact }) => {
+  const pathName = usePathname();
+
+  const active = exact ? pathName === href : pathName.startsWith(href);
+
+  return (
+    <Link href={href} className="group relative cursor-pointer">
+      <Label>{label}</Label>
+      <div
+        className={cn(
+          "absolute left-1/2 -translate-x-1/2 -bottom-1 h-1 w-1 rounded-full bg-neutral-200-text translate-y-1 opacity-0 transition",
+          { "translate-y-0 opacity-1": active, "group-hover:opacity-20 group-hover:translate-y-0": !active },
+        )}
+      ></div>
+    </Link>
   );
 };
