511a6ff22d983e717c417b691635153bc2cb11f0
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-04-24 09:33:51 +0200

    Added container component

---
+++ src/app/_components/primitives/container.tsx
@@ -0,0 +1,33 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { ComponentProps, FC, PropsWithChildren } from "react";
+import { cn } from "~/app/_utils/cn";
+
+export interface Props
+  extends ComponentProps<"div">,
+    VariantProps<typeof variants> {}
+
+const variants = cva("w-full mx-auto", {
+  variants: {
+    width: {
+      wide: "max-w-[calc(100vw-6px*2)] sm:max-w-[min(2200px,calc(100vw-2vw*2))]",
+      medium:
+        "max-w-[calc(100vw-12px*2)] sm:max-w-[min(1920px,calc(100vw-8vw*2))]",
+      narrow:
+        "max-w-[calc(100vw-12px*2)] sm:max-w-[min(800px,calc(100vw-8vw*2))]",
+    },
+  },
+  defaultVariants: { width: "medium" },
+});
+
+export const Container: FC<PropsWithChildren<Props>> = ({
+  width,
+  className,
+  children,
+  ...restProps
+}) => {
+  return (
+    <div className={cn(variants({ width }), className)} {...restProps}>
+      {children}
+    </div>
+  );
+};
