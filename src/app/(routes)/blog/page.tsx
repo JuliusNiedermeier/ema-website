8ffc5b770b20c0dea95efa634e40521add63e7c5
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-04-25 11:44:44 +0200

    Added some placeholder pages

---
+++ src/app/(routes)/blog/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { Container } from "~/app/_components/primitives/container";
+import { Heading } from "~/app/_components/primitives/typography";
+
+const BlogPage: FC = () => {
+  return (
+    <Container>
+      <Heading>Blog</Heading>
+    </Container>
+  );
+};
+
+export default BlogPage;
---
+++ src/app/(routes)/go/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { Container } from "~/app/_components/primitives/container";
+import { Heading } from "~/app/_components/primitives/typography";
+
+const GoPage: FC = () => {
+  return (
+    <Container>
+      <Heading>Anmeldung</Heading>
+    </Container>
+  );
+};
+
+export default GoPage;
