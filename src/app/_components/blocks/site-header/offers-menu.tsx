a81756d760ffdbb69c78bfa13156de43f3fcc3ae
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-04-25 11:45:56 +0200

    Created basic offer menu

---
+++ src/app/_components/blocks/site-header/offers-menu.tsx
@@ -0,0 +1,49 @@
+import { ComponentProps, FC } from "react";
+import Link from "next/link";
+import { cn } from "~/app/_utils/cn";
+import { Container } from "~/app/_components/primitives/container";
+import { Card } from "~/app/_components/primitives/card";
+import { Heading, Label, Paragraph } from "~/app/_components/primitives/typography";
+import { Button } from "~/app/_components/primitives/button";
+import { InteractionBubble } from "~/app/_components/compounds/interaction-bubble";
+
+export type OffersMenuProps = ComponentProps<typeof Container> & {};
+
+export const OffersMenu: FC<OffersMenuProps> = ({ className, ...restProps }) => {
+  return (
+    <Container
+      className={cn("flex flex-col-reverse xl:flex-row gap-4 sm:items-end py-4 border-t border-[gray]/20", className)}
+      {...restProps}
+    >
+      <Card className="w-full xl:max-w-96 bg-primary-900 text-neutral-900-text" data-animate>
+        <Heading size="sm">Finde heraus was zu Dir passt.</Heading>
+        <Paragraph className="text-neutral-900-text-muted">
+          Mit unserem Checkup tool findest du garantiert die perfekte Ausbildung für Dich!
+        </Paragraph>
+        <Button vairant="outline" size="sm" className="mt-8" href="/checkup">
+          <Label>Jetzt testen</Label>
+          <InteractionBubble />
+        </Button>
+      </Card>
+
+      <div className="sm:hidden">
+        <div className="text-right">
+          <Link href="/">
+            <Heading size="lg" tag="h3" data-animate className="!duration-300">
+              Home
+            </Heading>
+          </Link>
+          <Link href="/blog">
+            <Heading size="lg" tag="h3" data-animate className="!duration-150">
+              Blog
+            </Heading>
+          </Link>
+        </div>
+        <div className="h-px bg-neutral-400 my-8"></div>
+        <Heading size="lg" tag="h3" data-animate className="!duration-100">
+          Bildungswege
+        </Heading>
+      </div>
+    </Container>
+  );
+};
