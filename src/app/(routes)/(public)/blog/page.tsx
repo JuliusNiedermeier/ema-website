590dd5d86e3b0bbc399bd66135d82fc3f2cf76be
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-04-26 16:31:20 +0200

    Added latest post and post list to blog page

--- src/app/(routes)/(public)/blog/page.tsx
+++ src/app/(routes)/(public)/blog/page.tsx
@@ -1,27 +1,110 @@
 import { groq } from "next-sanity";
 import { FC } from "react";
 import { Container } from "~/app/_components/primitives/container";
-import { Heading } from "~/app/_components/primitives/typography";
+import { Label } from "~/app/_components/primitives/typography";
 import { sanity } from "~/sanity/lib/client";
+import { SparkleIcon } from "lucide-react";
+import Link from "next/link";
+import {
+  PostCard,
+  PostCardContent,
+  PostCardExcerpt,
+  PostCardMeta,
+  PostCardMetaDate,
+  PostCardMetaSeparator,
+  PostCardThumbnail,
+  PostCardThumbnailImage,
+  PostCardThumbnailTag,
+  PostCardTitle,
+} from "~/app/_components/primitives/post-card";
 import { PostsQueryResult } from "../../../../../generated/sanity/types";
+import { AuthorTag, AuthorTagImage, AuthorTagName } from "~/app/_components/primitives/author-tag";
 
 const postsQuery = groq`*[_type == "post"] | order(publishedAt desc) {
   _id,
   slug,
   title,
-  mainImage,
+  mainImage{asset->{url}},
   publishedAt,
   category->,
-  author->
+  author->{name, image{asset->{url}}},
+  excerpt
 }`;
 
 const BlogPage: FC = async () => {
-  const posts = await sanity.fetch<PostsQueryResult>(postsQuery);
+  const [latestPost, ...posts] = await sanity.fetch<PostsQueryResult>(postsQuery);
 
   return (
-    <Container>
-      <Heading>Blog</Heading>
-    </Container>
+    <>
+      <div className="bg-neutral-200 pb-16 pt-8">
+        <Container className="mt-4">
+          <div className="flex items-center gap-4">
+            <SparkleIcon />
+            <Label>Neu erschienen</Label>
+          </div>
+          <Link href={`/blog/${latestPost.slug?.current}`}>
+            <PostCard className="mt-4 block">
+              <PostCardThumbnail>
+                <PostCardThumbnailImage
+                  width={1920}
+                  height={1080}
+                  src={latestPost.mainImage?.asset?.url || ""}
+                  alt={latestPost.title || ""}
+                  className="max-h-[50vh]"
+                />
+                <PostCardThumbnailTag>{latestPost.category?.title}</PostCardThumbnailTag>
+              </PostCardThumbnail>
+              <PostCardTitle size="lg" className="max-w-[40rem]">
+                {latestPost.title}
+              </PostCardTitle>
+              <PostCardMeta>
+                <AuthorTag>
+                  <AuthorTagImage
+                    src={latestPost.author?.image?.asset?.url || ""}
+                    alt={latestPost.author?.name || "Author"}
+                  />
+                  <AuthorTagName>{latestPost.author?.name}</AuthorTagName>
+                </AuthorTag>
+                <PostCardMetaSeparator />
+                <PostCardMetaDate>
+                  {new Date(latestPost.publishedAt || "").toLocaleDateString("de", { dateStyle: "full" })}
+                </PostCardMetaDate>
+              </PostCardMeta>
+              <PostCardExcerpt className="max-w-[40rem]">{latestPost.excerpt}</PostCardExcerpt>
+            </PostCard>
+          </Link>
+        </Container>
+      </div>
+
+      <Container className="mt-8">
+        <div className="mt-8 grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] gap-x-4 gap-y-4">
+          {posts.map((post) => (
+            <Link href={`/blog/${post.slug?.current}`} className="-z-10">
+              <PostCard>
+                <PostCardThumbnail>
+                  <PostCardThumbnailImage src={post.mainImage?.asset?.url || ""} alt={post.title || ""} />
+                  <PostCardThumbnailTag>{post.category?.title}</PostCardThumbnailTag>
+                </PostCardThumbnail>
+                <PostCardContent>
+                  <PostCardTitle className="max-w-60">{post.title}</PostCardTitle>
+                  <PostCardMeta>
+                    <AuthorTag>
+                      <AuthorTagImage src={post.author?.image?.asset?.url || ""} alt={post.author?.name || "Author"} />
+                      <AuthorTagName>{post.author?.name}</AuthorTagName>
+                    </AuthorTag>
+                    <PostCardMetaSeparator />
+                    <PostCardMetaDate>
+                      {new Date(post.publishedAt || "").toLocaleDateString("de", { dateStyle: "full" })}
+                    </PostCardMetaDate>
+                  </PostCardMeta>
+                  <PostCardExcerpt>{post.excerpt}</PostCardExcerpt>
+                </PostCardContent>
+              </PostCard>
+            </Link>
+          ))}
+        </div>
+      </Container>
+    </>
   );
 };
 
