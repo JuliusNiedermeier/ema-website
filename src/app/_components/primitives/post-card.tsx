08637c84569c36a29e0b2e014c0d78babb43194a
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-04-26 16:30:41 +0200

    Created post card primitives

---
+++ src/app/_components/primitives/post-card.tsx
@@ -0,0 +1,71 @@
+import { ComponentProps, FC } from "react";
+import { cn } from "~/app/_utils/cn";
+import { Card } from "./card";
+import Image from "next/image";
+import { Chip } from "./chip";
+import { Heading, Label, Paragraph } from "./typography";
+
+export type PostCardProps = ComponentProps<typeof Card> & {};
+
+export const PostCard: FC<PostCardProps> = ({ className, ...restProps }) => {
+  return <Card className={cn("group p-2", className)} {...restProps} />;
+};
+
+export type PostCardThumbnailProps = ComponentProps<"div"> & {};
+
+export const PostCardThumbnail: FC<PostCardThumbnailProps> = ({ className, ...restProps }) => {
+  return <div className={cn("relative", className)} {...restProps} />;
+};
+
+export type PostCardThumbnailImageProps = ComponentProps<typeof Image> & {};
+
+export const PostCardThumbnailImage: FC<PostCardThumbnailImageProps> = ({
+  className,
+  width = 500,
+  height = 500,
+  ...restProps
+}) => {
+  return <Image className={cn("rounded-xl object-cover", className)} width={width} height={height} {...restProps} />;
+};
+
+export type PostCardThumbnailTagProps = ComponentProps<typeof Chip> & {};
+
+export const PostCardThumbnailTag: FC<PostCardThumbnailTagProps> = ({ className, ...restProps }) => {
+  return <Chip className={cn("absolute bottom-2 left-2", className)} {...restProps} />;
+};
+
+export type PostCardTitleProps = ComponentProps<typeof Heading> & {};
+
+export const PostCardTitle: FC<PostCardTitleProps> = ({ className, size = "sm", ...restProps }) => {
+  return <Heading size={size} className={cn("", className)} {...restProps} />;
+};
+
+export type PostCardMetaProps = ComponentProps<"div"> & {};
+
+export const PostCardMeta: FC<PostCardMetaProps> = ({ className, ...restProps }) => {
+  return <div className={cn("mt-2 flex items-center gap-2", className)} {...restProps} />;
+};
+
+export type PostCardMetaDateProps = ComponentProps<typeof Label> & {};
+
+export const PostCardMetaDate: FC<PostCardMetaDateProps> = ({ className, ...restProps }) => {
+  return <Label className={cn("whitespace-nowrap", className)} {...restProps} />;
+};
+
+export type PostCardMetaSeparatorProps = ComponentProps<"span"> & {};
+
+export const PostCardMetaSeparator: FC<PostCardMetaSeparatorProps> = ({ className, ...restProps }) => {
+  return <span className={cn("h-1 w-1 rounded-full bg-neutral-200-text-muted", className)} {...restProps} />;
+};
+
+export type PostCardExcerptProps = ComponentProps<typeof Paragraph> & {};
+
+export const PostCardExcerpt: FC<PostCardExcerptProps> = ({ className, ...restProps }) => {
+  return <Paragraph className={cn("text-neutral-200-text-muted", className)} {...restProps} />;
+};
+
+export type PostCardContentProps = ComponentProps<"div"> & {};
+
+export const PostCardContent: FC<PostCardContentProps> = ({ className, ...restProps }) => {
+  return <div className={cn("p-6", className)} {...restProps} />;
+};
