73ea249ce0e7b0540a0a473fab4a7ffd9e547d23
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-04-26 15:18:30 +0200

    Added posts requests to blog page

--- src/app/(routes)/(public)/blog/page.tsx
+++ src/app/(routes)/(public)/blog/page.tsx
@@ -1,8 +1,23 @@
+import { groq } from "next-sanity";
 import { FC } from "react";
 import { Container } from "~/app/_components/primitives/container";
 import { Heading } from "~/app/_components/primitives/typography";
+import { sanity } from "~/sanity/lib/client";
+import { PostsQueryResult } from "../../../../../generated/sanity/types";
 
-const BlogPage: FC = () => {
+const postsQuery = groq`*[_type == "post"] | order(publishedAt desc) {
+  _id,
+  slug,
+  title,
+  mainImage,
+  publishedAt,
+  category->,
+  author->
+}`;
+
+const BlogPage: FC = async () => {
+  const posts = await sanity.fetch<PostsQueryResult>(postsQuery);
+
   return (
     <Container>
       <Heading>Blog</Heading>
