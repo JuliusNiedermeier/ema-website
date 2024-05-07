52650b4e67edae8ca3f0f0a5349aa6a11c15e8f6
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-05-07 22:22:48 +0200

    Created testimonial carousel  block component with cms data

---
+++ src/app/_components/blocks/testimonial-carousel.tsx
@@ -0,0 +1,30 @@
+import { ComponentProps, FC } from "react";
+import { TestimonialCarousel as TestimonialCarouselCompound } from "../compounds/testimonial-carousel";
+import { groq } from "next-sanity";
+import { sanity } from "~/sanity/lib/client";
+import { TestimonialsQueryResult } from "../../../../generated/sanity/types";
+
+const testimonialsQuery = groq`*[_type == "testimonial"]{
+    ...,
+    authorImage{asset->{url}}
+}`;
+
+export type TestimonialCarouselProps = Omit<ComponentProps<typeof TestimonialCarouselCompound>, "testimonials"> & {};
+
+export const TestimonialCarousel: FC<TestimonialCarouselProps> = async ({ ...restProps }) => {
+  const tesimonials = await sanity.fetch<TestimonialsQueryResult>(testimonialsQuery);
+
+  return (
+    <TestimonialCarouselCompound
+      testimonials={tesimonials.map<ComponentProps<typeof TestimonialCarouselCompound>["testimonials"][number]>(
+        (testimonial) => ({
+          rating: testimonial.stars || 0,
+          body: testimonial.testimonial || "",
+          authorName: testimonial.authorName || "Anonym",
+          authorImage: testimonial.authorImage?.asset?.url || "",
+        }),
+      )}
+      {...restProps}
+    />
+  );
+};
