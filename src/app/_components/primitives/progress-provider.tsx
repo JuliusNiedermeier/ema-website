e8d24b66d938d1ed7960a8748941b79cd1ee0855
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-05-07 22:18:47 +0200

    Created reusable progress provider

---
+++ src/app/_components/primitives/progress-provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Dispatch, FC, PropsWithChildren, SetStateAction, createContext, useContext, useState } from "react";
+
+type ProgressContext = { progress: number; setProgress: Dispatch<SetStateAction<number>> };
+
+const ProgressContext = createContext<ProgressContext>({ progress: 0, setProgress: () => {} });
+
+export const useProgress = () => {
+  const context = useContext(ProgressContext);
+  if (!context) throw new Error("Hook useProgress must be used inside a <ProgressProvider>.");
+  return context;
+};
+
+export const ProgressProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [progress, setProgress] = useState(0);
+  return <ProgressContext.Provider value={{ progress, setProgress }} children={children} />;
+};
