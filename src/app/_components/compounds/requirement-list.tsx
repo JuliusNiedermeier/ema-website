305ea181c94a8ebe2e221b66fc76f820047a5e20
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-05-11 19:19:44 +0200

    Added missing key prop

--- src/app/_components/compounds/requirement-list.tsx
+++ src/app/_components/compounds/requirement-list.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, FC } from "react";
+import { ComponentProps, FC, Fragment } from "react";
 import { cn } from "~/app/_utils/cn";
 import { Label } from "../primitives/typography";
 import { CheckIcon } from "lucide-react";
@@ -11,10 +11,10 @@ export const RequirementList: FC<RequirementListProps> = ({ className, groups, .
   return (
     <div className={cn("flex flex-col justify-center gap-8 sm:flex-row", className)} {...restProps}>
       {groups.map((group, index, array) => (
-        <>
+        <Fragment key={index}>
           <div className="flex max-w-[40rem] flex-col gap-2">
-            {group.map((requirement) => (
-              <div className="flex items-center gap-4 rounded-full bg-themed-primary p-2 pr-8">
+            {group.map((requirement, index) => (
+              <div key={index} className="flex items-center gap-4 rounded-full bg-themed-primary p-2 pr-8">
                 <div className="rounded-full bg-primary-900 p-2 text-neutral-100">
                   <CheckIcon />
                 </div>
@@ -29,7 +29,7 @@ export const RequirementList: FC<RequirementListProps> = ({ className, groups, .
               <div className="h-px flex-1 bg-neutral-400 sm:w-px" />
             </div>
           )}
-        </>
+        </Fragment>
       ))}
     </div>
   );
