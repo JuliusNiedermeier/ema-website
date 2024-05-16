9b7195b1a0e0e8c59c9cf465541e8f3e1d3e4cda
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-05-16 23:43:38 +0200

    Improved programs step design

--- src/app/_components/compounds/application-form/steps/programs.tsx
+++ src/app/_components/compounds/application-form/steps/programs.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { FC } from "react";
 import { Heading, Label, Paragraph } from "~/app/_components/primitives/typography";
 import { OfferCard } from "~/app/_components/primitives/offers/offers";
 import { Chip } from "~/app/_components/primitives/chip";
 import { cn } from "~/app/_utils/cn";
 import { useApplicationFormState } from "../state";
+import { StepIcon } from "~/app/_components/primitives/step-list";
+import { CheckIcon } from "lucide-react";
 
 export type ProgramsStepProps = {
   programs: {
@@ -19,31 +23,31 @@ export const ProgramsStep: FC<ProgramsStepProps> = ({ programs }) => {
   const { program: selectedProgram, setProgram } = useApplicationFormState();
 
   return (
-    <div className="text-center">
-      <Heading>Wähle einen Bildungsweg</Heading>
-      <Paragraph>Die Wahl deines Bildungsweges ist sehr wichtig.</Paragraph>
-      <div className="mt-16 grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] gap-4">
+    <div>
+      <Heading>Für welchen Bildungsgang möchtest du dich anmelden?</Heading>
+      <div className="mt-16 flex flex-col gap-4">
         {programs.map((program, index) => (
           <button key={index} onClick={() => setProgram(program.ID)}>
             <OfferCard
               key={index}
-              className={cn("h-full text-left", {
-                "ring ring-primary-900": program.ID === selectedProgram,
-              })}
+              className={cn("flex h-full items-center gap-4 overflow-hidden text-left")}
               style={{
                 backgroundColor: program.colors?.primary,
-                opacity: program.ID === selectedProgram ? 1 : 0.8,
               }}
             >
-              <Label>{program.programType.name}</Label>
-              <Heading size="sm" className="mb-0 mt-0">
-                {program.name}
-              </Heading>
-              {program.variant && (
-                <Chip style={{ backgroundColor: program.colors.secondary }} className="mt-2">
-                  {program.variant}
-                </Chip>
-              )}
+              <div className="flex-1">
+                <Label>{program.programType.name}</Label>
+                <Heading size="sm" className="mb-0 mt-0">
+                  {program.name}
+                </Heading>
+                {program.variant && <Label>{program.variant}</Label>}
+              </div>
+              <StepIcon
+                status="complete"
+                className={cn("transition-all", { "translate-x-[200%] rotate-90": program.ID !== selectedProgram })}
+              >
+                <CheckIcon />
+              </StepIcon>
             </OfferCard>
           </button>
         ))}
