d6cdb5857131a564858c9c7a240aa2644b51a527
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-06-09 15:21:07 +0200

    Added reduced motion variant to InteractionBubble

--- src/app/_components/compounds/interaction-bubble.tsx
+++ src/app/_components/compounds/interaction-bubble.tsx
@@ -2,18 +2,31 @@ import { ArrowRightIcon } from "lucide-react";
 import { ComponentProps, FC } from "react";
 import { cn } from "~/app/_utils/cn";
 
-export type InteractionBubbleProps = ComponentProps<"div"> & {};
+export type InteractionBubbleProps = ComponentProps<"div"> & {
+  animated?: boolean;
+};
 
-export const InteractionBubble: FC<InteractionBubbleProps> = ({ className, ...restProps }) => {
+export const InteractionBubble: FC<InteractionBubbleProps> = ({ className, animated = true, ...restProps }) => {
   return (
     <div
       className={cn(
-        "relative inline-block h-0 w-0 overflow-hidden rounded-full bg-primary-900 text-primary-900-text opacity-0 transition-all duration-200 group-hover:h-8 group-hover:w-8 group-hover:opacity-100",
+        "relative inline-block h-8 w-8 overflow-hidden rounded-full bg-primary-900 text-primary-900-text",
+        {
+          "h-0 w-0 opacity-0 transition-all duration-200 group-hover:h-8 group-hover:w-8 group-hover:opacity-100":
+            animated,
+        },
         className,
       )}
       {...restProps}
     >
-      <ArrowRightIcon className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-180 transition-all duration-300 group-hover:-rotate-45" />
+      <ArrowRightIcon
+        className={cn(
+          "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-all duration-300 group-hover:-rotate-45",
+          {
+            "-rotate-180": animated,
+          },
+        )}
+      />
     </div>
   );
 };
