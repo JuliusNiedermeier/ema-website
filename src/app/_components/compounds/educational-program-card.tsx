284a3a12ad6886a9f13c9cbacefcc03ba35a8268
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-06-09 15:36:05 +0200

    Created reusable EducationalProgramCard component

---
+++ src/app/_components/compounds/educational-program-card.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps, FC } from "react";
+import { cn } from "~/app/_utils/cn";
+import { Card } from "../primitives/card";
+import { Chip } from "../primitives/chip";
+import { Heading, Label, Paragraph } from "../primitives/typography";
+import { InteractionBubble } from "./interaction-bubble";
+
+export type EducationalProgramCardProps = ComponentProps<typeof Card> & {
+  programType: string;
+  name: string;
+  headline: string;
+};
+
+export const EducationalProgramCard: FC<EducationalProgramCardProps> = ({
+  className,
+  programType,
+  name,
+  headline,
+  ...restProps
+}) => {
+  return (
+    <Card className={cn("group flex flex-col bg-themed-primary", className)} {...restProps}>
+      <Chip className="bg-themed-secondary">
+        <Label>{programType}</Label>
+      </Chip>
+      <Heading size="lg" className="mt-8 text-balance">
+        {name}
+      </Heading>
+      <Paragraph className="flex-1 text-balance">{headline}</Paragraph>
+      <InteractionBubble animated={false} className="mt-8" />
+    </Card>
+  );
+};
