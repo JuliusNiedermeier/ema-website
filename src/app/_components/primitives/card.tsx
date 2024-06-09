259ea74c61cbb4d4d01c6624224e20162525c7e2
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-06-09 15:15:03 +0200

    Added asChild prpp to Card component

--- src/app/_components/primitives/card.tsx
+++ src/app/_components/primitives/card.tsx
@@ -1,13 +1,17 @@
+import { Slot } from "@radix-ui/react-slot";
 import { ComponentProps, FC, forwardRef } from "react";
 import { cn } from "~/app/_utils/cn";
 
-export type CardProps = ComponentProps<"div"> & {};
+export type CardProps = ComponentProps<"div"> & {
+  asChild?: boolean;
+};
 
-export const Card: FC<CardProps> = forwardRef(({ className, children, ...restProps }, ref) => {
+export const Card: FC<CardProps> = forwardRef(({ className, children, asChild, ...restProps }, ref) => {
+  const Component = asChild ? Slot : "div";
   return (
-    <div ref={ref} className={cn("rounded-2xl bg-neutral-200 p-8", className)} {...restProps}>
+    <Component ref={ref} className={cn("rounded-2xl bg-neutral-200 p-8", className)} {...restProps}>
       {children}
-    </div>
+    </Component>
   );
 });
 
