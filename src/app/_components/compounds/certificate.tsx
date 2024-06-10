dba2446270b74ea781aa145ba1d722f6972c1223
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-06-10 13:26:18 +0200

    Added scroll animation to certificate component

--- src/app/_components/compounds/certificate.tsx
+++ src/app/_components/compounds/certificate.tsx
@@ -1,9 +1,12 @@
-import { ComponentProps, FC } from "react";
+"use client";
+
+import { ComponentProps, FC, useRef } from "react";
 import { cn } from "~/app/_utils/cn";
 import { Card } from "../primitives/card";
 import { Heading, Label, Paragraph } from "../primitives/typography";
 import { CheckIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export type CertificateProps = ComponentProps<typeof Card> & {
   heading: string;
@@ -22,8 +25,14 @@ export const Certificate: FC<CertificateProps> = ({
   jobs,
   ...restProps
 }) => {
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({ axis: "y", target: cardRef, offset: ["start end", "end start"] });
+  const paralaxx = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
   return (
     <Card
+      ref={cardRef}
       className={cn("grid items-center gap-16 overflow-hidden py-0 sm:grid-cols-[2fr_1fr]", className)}
       {...restProps}
     >
@@ -47,7 +56,10 @@ export const Certificate: FC<CertificateProps> = ({
         </div>
       </div>
       <div className="relative h-full min-h-[30rem] overflow-hidden">
-        <div className="absolute left-0 top-0 flex h-full w-full flex-col gap-4">
+        <motion.div
+          className="absolute left-0 top-0 flex h-full w-full flex-col justify-center gap-4"
+          style={{ y: paralaxx }}
+        >
           {jobs.map((job, index) => (
             <div key={index} className="flex gap-4 rounded-[2rem] border border-neutral-400 bg-neutral-100 p-4">
               <Image
@@ -60,7 +72,7 @@ export const Certificate: FC<CertificateProps> = ({
               <Paragraph>{job.content}</Paragraph>
             </div>
           ))}
-        </div>
+        </motion.div>
         <div className="left- 0 absolute top-0 h-full w-full bg-gradient-to-b from-neutral-200 to-transparent sm:hidden"></div>
       </div>
     </Card>
