fe81476328a1040c5356a3872c7e64459c8162ef
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-06-17 14:26:18 +0200

    Created alternative campus card

---
+++ src/app/_components/blocks/campus-card-legacy.tsx
@@ -0,0 +1,95 @@
+import { ComponentProps, FC } from "react";
+import { cn } from "~/app/_utils/cn";
+import { Heading, Label, Paragraph } from "../primitives/typography";
+import { sanity } from "~/sanity/lib/client";
+import { groq } from "next-sanity";
+import { CampusCardQueryResult } from "../../../../generated/sanity/types";
+import { Card } from "../primitives/card";
+import { InteractionBubble } from "../compounds/interaction-bubble";
+import Image from "next/image";
+
+const campusCardQuery = groq`*[_type == "campus-page"][0] {
+    ...,
+    heroImage { asset -> { url } },
+    images[] { asset -> { url } },
+    team[] {
+        ...,
+        image { asset -> { url } }
+    }
+}`;
+
+export type CampusCardProps = ComponentProps<"div"> & {};
+
+export const CampusCard: FC<CampusCardProps> = async ({ className, ...restProps }) => {
+  const data = await sanity.fetch<CampusCardQueryResult>(campusCardQuery);
+
+  return (
+    <div
+      className={cn(
+        "group grid cursor-pointer gap-2 overflow-hidden rounded-3xl lg:grid-cols-[1fr_min-content_1fr_1fr_1fr] lg:grid-rows-[14rem_10rem_1fr] lg:gap-4",
+        className,
+      )}
+      {...restProps}
+    >
+      <Image
+        src={data?.images?.[0].asset?.url || ""}
+        height={500}
+        width={500}
+        alt="Bild 1"
+        className="h-full w-full rounded-2xl object-cover transition-transform duration-500 group-hover:-translate-x-4 group-hover:-translate-y-4"
+      />
+      <div className="grid aspect-square grid-cols-3 grid-rows-3 gap-4">
+        {data?.team
+          ?.slice(0, 9)
+          .map((teamMember, index) => (
+            <Image
+              key={index}
+              src={teamMember.image?.asset?.url || ""}
+              height={100}
+              width={100}
+              alt={teamMember.name || ""}
+              className="aspect-square rounded-full bg-neutral-200 object-cover"
+            />
+          ))}
+      </div>
+      <Image
+        src={data?.heroImage?.asset?.url || ""}
+        height={500}
+        width={500}
+        alt="Bild 2"
+        className="col-span-2 h-full w-full rounded-2xl object-cover transition-transform duration-500 group-hover:-translate-y-4"
+      />
+      <Image
+        src={data?.images?.[1].asset?.url || ""}
+        height={500}
+        width={500}
+        alt="Bild 3"
+        className="h-full w-full rounded-2xl object-cover transition-transform duration-500 group-hover:-translate-y-4 group-hover:translate-x-4"
+      />
+      <Image
+        src={data?.images?.[2].asset?.url || ""}
+        height={500}
+        width={500}
+        alt="Bild 4"
+        className="col-span-2 h-full w-full rounded-2xl object-cover transition-transform duration-500 group-hover:-translate-x-4"
+      />
+      <div className="col-span-2 row-span-2 min-w-96 p-4">
+        <Heading>{data?.heading}</Heading>
+        <Paragraph>{data?.previewText}</Paragraph>
+      </div>
+      <Image
+        src={data?.images?.[3].asset?.url || ""}
+        height={500}
+        width={500}
+        alt="Bild 5"
+        className="h-full w-full rounded-2xl object-cover transition-transform duration-500 group-hover:translate-x-4"
+      />
+      <div className="col-span-2 p-8">
+        <div className="flex h-8 items-center gap-4">
+          <InteractionBubble />
+          <Label>Mehr lesen</Label>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/app/_components/blocks/campus-card.tsx
+++ src/app/_components/blocks/campus-card.tsx
@@ -24,72 +24,22 @@ export const CampusCard: FC<CampusCardProps> = async ({ className, ...restProps 
   const data = await sanity.fetch<CampusCardQueryResult>(campusCardQuery);
 
   return (
-    <div
-      className={cn(
-        "group grid cursor-pointer gap-2 overflow-hidden rounded-3xl lg:grid-cols-[1fr_min-content_1fr_1fr_1fr] lg:grid-rows-[14rem_10rem_1fr] lg:gap-4",
-        className,
-      )}
-      {...restProps}
-    >
-      <Image
-        src={data?.images?.[0].asset?.url || ""}
-        height={500}
-        width={500}
-        alt="Bild 1"
-        className="h-full w-full rounded-2xl object-cover transition-transform duration-500 group-hover:-translate-x-4 group-hover:-translate-y-4"
-      />
-      <div className="grid aspect-square grid-cols-3 grid-rows-3 gap-4">
-        {data?.team
-          ?.slice(0, 9)
-          .map((teamMember, index) => (
-            <Image
-              key={index}
-              src={teamMember.image?.asset?.url || ""}
-              height={100}
-              width={100}
-              alt={teamMember.name || ""}
-              className="aspect-square rounded-full bg-neutral-200 object-cover"
-            />
-          ))}
-      </div>
+    <Card className={cn("group relative overflow-hidden bg-transparent p-0", className)} {...restProps}>
       <Image
         src={data?.heroImage?.asset?.url || ""}
-        height={500}
-        width={500}
-        alt="Bild 2"
-        className="col-span-2 h-full w-full rounded-2xl object-cover transition-transform duration-500 group-hover:-translate-y-4"
+        height={1000}
+        width={1000}
+        alt="Campus"
+        className="absolute left-0 top-0 -z-10 h-full w-full object-cover"
       />
-      <Image
-        src={data?.images?.[1].asset?.url || ""}
-        height={500}
-        width={500}
-        alt="Bild 3"
-        className="h-full w-full rounded-2xl object-cover transition-transform duration-500 group-hover:-translate-y-4 group-hover:translate-x-4"
-      />
-      <Image
-        src={data?.images?.[2].asset?.url || ""}
-        height={500}
-        width={500}
-        alt="Bild 4"
-        className="col-span-2 h-full w-full rounded-2xl object-cover transition-transform duration-500 group-hover:-translate-x-4"
-      />
-      <div className="col-span-2 row-span-2 min-w-96 p-4">
+      <Card className="m-2 mt-64 max-w-[40rem] bg-neutral-100 lg:m-4 lg:mt-64">
         <Heading>{data?.heading}</Heading>
         <Paragraph>{data?.previewText}</Paragraph>
-      </div>
-      <Image
-        src={data?.images?.[3].asset?.url || ""}
-        height={500}
-        width={500}
-        alt="Bild 5"
-        className="h-full w-full rounded-2xl object-cover transition-transform duration-500 group-hover:translate-x-4"
-      />
-      <div className="col-span-2 p-8">
-        <div className="flex h-8 items-center gap-4">
-          <InteractionBubble />
-          <Label>Mehr lesen</Label>
+        <div className="mt-8 flex items-center gap-4">
+          <InteractionBubble animated={false} />
+          <Label>Deinen Campus kennenlernen</Label>
         </div>
-      </div>
-    </div>
+      </Card>
+    </Card>
   );
 };
