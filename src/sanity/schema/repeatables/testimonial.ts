59c17d7f55d39e97ed0d7bd214481f1514795fc3
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-06-27 11:06:51 +0200

    Added validation and descriptions to testimonial cms type

--- src/sanity/schema/repeatables/testimonial.ts
+++ src/sanity/schema/repeatables/testimonial.ts
@@ -7,10 +7,42 @@ export const testimonial: SchemaTypeDef = {
     title: "Testimonial",
     type: "document",
     fields: [
-      defineField({ name: "stars", title: "Sternebewertung", type: "number" }),
-      defineField({ name: "testimonial", title: "Zitat", type: "text" }),
-      defineField({ name: "authorImage", title: "Bild des Autors", type: "image" }),
-      defineField({ name: "authorName", title: "Name des Authors", type: "string" }),
+      defineField({
+        name: "stars",
+        title: "Sternebewertung",
+        description: "Testimonials mit weniger als 3 Sternen sollten nicht auf der Seite platziert werden.",
+        type: "number",
+        options: { list: [3, 4, 5], layout: "radio" },
+        validation: (r) => r.required(),
+      }),
+      defineField({
+        name: "testimonial",
+        title: "Text",
+        description: "100-500 Zeichen",
+        type: "text",
+        validation: (r) => r.required().min(100).max(500),
+      }),
+      defineField({
+        name: "authorName",
+        title: "Name des Autors",
+        description: "3-30 Zeichen",
+        type: "string",
+        validation: (r) => r.required().min(3).max(30),
+      }),
+      defineField({
+        name: "authorImage",
+        title: "Bild des Autors",
+        description: "Optional. Alternativ werden die Initialen des Autors angezeigt.",
+        type: "image",
+        validation: (r) => r,
+      }),
     ],
+    preview: {
+      select: { title: "testimonial", subtitle: "authorName" },
+      prepare: ({ title, subtitle }) => ({
+        title: `${(title satisfies string).slice(0, 30)}...`,
+        subtitle,
+      }),
+    },
   }),
 };
