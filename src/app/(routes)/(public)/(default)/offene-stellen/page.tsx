a34118344b963bdf216b6e2c62b867d3c9794660
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-07-09 22:07:39 +0200

    Added list of jobs as an accordion to the jobs page. Populated with cms data

--- src/app/(routes)/(public)/(default)/offene-stellen/page.tsx
+++ src/app/(routes)/(public)/(default)/offene-stellen/page.tsx
@@ -1,16 +1,62 @@
+import { groq } from "next-sanity";
 import { FC } from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemContent,
+  AccordionItemTrigger,
+  AccordionItemTriggerIndicator,
+} from "~/app/_components/primitives/accordion";
 import { Container } from "~/app/_components/primitives/container";
-import { Heading, Paragraph } from "~/app/_components/primitives/typography";
+import { Heading, Label, Paragraph } from "~/app/_components/primitives/typography";
+import { sanity } from "~/sanity/lib/client";
+import { JobsPageQueryResult } from "../../../../../../generated/sanity/types";
+import { Button, ButtonInteractionBubble } from "~/app/_components/primitives/button";
+import { Chip } from "~/app/_components/primitives/chip";
 
-const JobsPage: FC = () => {
+const jobsPageQuery = groq`*[_type == "jobs-page"][0]`;
+
+const JobsPage: FC = async () => {
+  const data = await sanity.fetch<JobsPageQueryResult>(jobsPageQuery, {}, { next: { tags: ["jobs-page"] } });
+
   return (
-    <Container width="narrow" className="my-32 text-center">
-      <Heading>Wir suchen Dich!</Heading>
-      <Paragraph>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor assumenda obcaecati aperiam, nisi quia
-        voluptatibus possimus maxime similique tempora dicta quos molestias sed reprehenderit quis sapiente optio
-        voluptas quam aliquam!
-      </Paragraph>
+    <Container width="narrow" className="my-32">
+      <div className="text-center">
+        <Heading>{data?.heading}</Heading>
+        <Paragraph>{data?.description}</Paragraph>
+      </div>
+
+      <Accordion className="mt-16">
+        {data?.jobs?.map((job, index) => (
+          <AccordionItem key={index} index={index}>
+            <AccordionItemTrigger>
+              <div className="w-full py-2">
+                <div className="mt-2 flex items-center gap-4">
+                  <Heading size="sm" className="my-0">
+                    {job.title}
+                  </Heading>
+                  <AccordionItemTriggerIndicator className="ml-auto" />
+                </div>
+                <Label className="text-neutral-100-text-muted">{job.shortDescription}</Label>
+                {job.isTeacherJob && (
+                  <Chip className="mt-2 bg-primary-900 text-neutral-900-text">
+                    <Label>{data.teachingJobLabel}</Label>
+                  </Chip>
+                )}
+              </div>
+            </AccordionItemTrigger>
+            <AccordionItemContent>
+              <div>
+                <Paragraph className="my-0">{job.fullDescription}</Paragraph>
+                <Button size="sm" vairant="outline" className="mt-4 transition-[padding] hover:pr-1">
+                  <Label>{data.CTALabel}</Label>
+                  <ButtonInteractionBubble />
+                </Button>
+              </div>
+            </AccordionItemContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </Container>
   );
 };
