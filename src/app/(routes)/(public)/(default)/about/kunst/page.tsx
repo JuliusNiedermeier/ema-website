2d88fc018cc9dd6b52cc344c74c11224ec2878b4
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-07-12 16:17:15 +0200

    Renamed team page to art page

--- src/app/(routes)/(public)/(default)/about/kunst/page.tsx
+++ src/app/(routes)/(public)/(default)/about/kunst/page.tsx
@@ -2,10 +2,10 @@ import { FC } from "react";
 import { Container } from "~/app/_components/primitives/container";
 import { Heading, Paragraph } from "~/app/_components/primitives/typography";
 
-const TeamPage: FC = () => {
+const ArtPage: FC = () => {
   return (
     <Container width="narrow" className="my-32 text-center">
-      <Heading>Das Team</Heading>
+      <Heading>Kunst</Heading>
       <Paragraph>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor assumenda obcaecati aperiam, nisi quia
         voluptatibus possimus maxime similique tempora dicta quos molestias sed reprehenderit quis sapiente optio
@@ -15,4 +15,4 @@ const TeamPage: FC = () => {
   );
 };
 
-export default TeamPage;
+export default ArtPage;
