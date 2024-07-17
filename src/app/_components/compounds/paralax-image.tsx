8f6135233bc82b2c31a25368b8971d87a6db01bc
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-07-17 23:32:24 +0200

    Added scale and shift props for paralax-image

--- src/app/_components/compounds/paralax-image.tsx
+++ src/app/_components/compounds/paralax-image.tsx
@@ -1,21 +1,35 @@
 "use client";
 
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { ComponentProps, FC, useRef } from "react";
 import { cn } from "~/app/_utils/cn";
 
-export type ParalaxContainerProps = ComponentProps<typeof motion.div> & {};
+export type ParalaxContainerProps = ComponentProps<typeof motion.div> & {
+  shift?: number;
+  scale?: number;
+};
 
-export const ParalaxContainer: FC<ParalaxContainerProps> = ({ className, children, ...restProps }) => {
+export const ParalaxContainer: FC<ParalaxContainerProps> = ({
+  className,
+  children,
+  shift = 200,
+  scale = 1.2,
+  ...restProps
+}) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  const { scrollYProgress } = useScroll({ target: containerRef, offset: ["start end", "end start"] });
+  const { scrollYProgress } = useScroll({ target: containerRef, offset: ["start end", "end start"], smooth: 1 });
 
-  const yOffset = useTransform(scrollYProgress, [0, 1], [-300, 300]);
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 1.2]);
+  const yOffsetValue = useTransform(scrollYProgress, [0, 1], [-shift, shift]);
+  const scaleValue = useTransform(scrollYProgress, [0, 1], [1, scale]);
 
   return (
-    <motion.div ref={containerRef} className={cn("", className)} style={{ y: yOffset, scale }} {...restProps}>
+    <motion.div
+      ref={containerRef}
+      className={cn("", className)}
+      style={{ y: yOffsetValue, scale: scaleValue }}
+      {...restProps}
+    >
       {children}
     </motion.div>
   );
