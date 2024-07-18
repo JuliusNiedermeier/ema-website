6543dd330404e2538acac8e2fb9727c7d509a1a5
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-07-18 15:49:30 +0200

    Refactored campus-info-card to close other cards when opening a card.
    Also removed sticky positioning and improved spacing.

--- src/app/(routes)/(public)/(default)/about/campus/page.tsx
+++ src/app/(routes)/(public)/(default)/about/campus/page.tsx
@@ -6,7 +6,7 @@ import { sanity } from "~/sanity/lib/client";
 import { CampusPageQueryResult } from "../../../../../../../generated/sanity/types";
 import Image from "next/image";
 import { ParalaxContainer } from "~/app/_components/compounds/paralax-image";
-import { CampusInfoCard } from "~/app/_components/compounds/campus-info-card";
+import { CampusInfoCard, CampusInfoCardListProvider } from "~/app/_components/compounds/campus-info-card";
 
 const campusPageQuery = groq`*[_type == "campus-page"][0]{
   ...,
@@ -29,28 +29,36 @@ const CampusPage: FC = async () => {
         <Paragraph className="max-w-[40rem]">{data?.previewText}</Paragraph>
       </Container>
       <div className="flex flex-col gap-4">
-        {data?.images?.map((image, index, images) => (
-          <div key={index} className="relative h-[75vh]">
-            <div className="absolute left-0 top-0 h-full w-full">
-              <Container width="wide" className="h-full overflow-hidden rounded-3xl">
-                <ParalaxContainer className="h-full w-full">
-                  <Image
-                    src={image.asset?.url || ""}
-                    alt={"TODO"}
-                    width="1920"
-                    height="1080"
-                    className="h-full w-full overflow-hidden object-cover"
-                  />
-                </ParalaxContainer>
-              </Container>
+        <CampusInfoCardListProvider>
+          {data?.images?.map((image, index, images) => (
+            <div key={index} className="relative h-[75vh]">
+              <div className="absolute left-0 top-0 h-full w-full">
+                <Container width="wide" className="h-full overflow-hidden rounded-3xl">
+                  <ParalaxContainer className="h-full w-full">
+                    <Image
+                      src={image.asset?.url || ""}
+                      alt={"TODO"}
+                      width="1920"
+                      height="1080"
+                      className="h-full w-full overflow-hidden object-cover"
+                    />
+                  </ParalaxContainer>
+                </Container>
+              </div>
+              <div className="absolute bottom-0 left-0 w-full">
+                <Container className="py-[6px] md:py-8">
+                  <CampusInfoCard title={`Teammitglied ${index + 1}`} subtitle="Sportlehrer">
+                    <Paragraph>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt expedita laborum, soluta eum
+                      aliquid enim, cum facilis sunt minus facere voluptatum iure nostrum fugit aliquam blanditiis
+                      veritatis porro sint sapiente.
+                    </Paragraph>
+                  </CampusInfoCard>
+                </Container>
+              </div>
             </div>
-            <div className="absolute left-0 top-0 flex h-full w-full items-end">
-              <Container className="sticky bottom-0 py-[6px] md:py-8">
-                <CampusInfoCard />
-              </Container>
-            </div>
-          </div>
-        ))}
+          ))}
+        </CampusInfoCardListProvider>
       </div>
     </div>
   );
--- src/app/_components/compounds/campus-info-card.tsx
+++ src/app/_components/compounds/campus-info-card.tsx
@@ -1,114 +1,125 @@
 "use client";
 
-import { ComponentProps, FC, useRef, useState } from "react";
+import {
+  ComponentProps,
+  createContext,
+  Dispatch,
+  FC,
+  PropsWithChildren,
+  SetStateAction,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { cn } from "~/app/_utils/cn";
 import { Heading, Label, Paragraph } from "../primitives/typography";
 import { ArrowUpRightIcon } from "lucide-react";
 
-export type CampusInfoCardProps = ComponentProps<"div"> & {};
+let lastID = 0;
+
+const createID = () => (lastID++).toString();
+
+type AnimationState = "expanding" | "expanded" | "closing" | "closed";
+
+export type CampusInfoCardProps = ComponentProps<"div"> & {
+  title: string;
+  subtitle: string;
+};
 
 const maxWidth = 500;
+const animationDuration = 150;
 
-export const CampusInfoCard: FC<CampusInfoCardProps> = ({ className, onClick, ...restProps }) => {
-  const [state, setState] = useState<"will-open" | "open" | "will-close" | "closed">("closed");
-
+export const CampusInfoCard: FC<CampusInfoCardProps> = ({
+  className,
+  onClick,
+  title,
+  subtitle,
+  children,
+  ...restProps
+}) => {
+  const { expandedID, setExpandedID } = useCampusInfoCardList();
+  const IDRef = useRef(createID());
   const containerRef = useRef<HTMLDivElement>(null);
   const contentWindowRef = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
 
+  const active = expandedID === IDRef.current;
+
+  const [animationState, setAnimationState] = useState<AnimationState>(active ? "expanded" : "closed");
+
   const handleCardClick: ComponentProps<"div">["onClick"] = (e) => {
-    state === "open" || state === "will-open" ? closeCard() : openCard();
+    setExpandedID(active ? null : IDRef.current);
     onClick?.(e);
   };
 
-  const openCard = () => {
+  const expand = () => {
     if (!containerRef.current || !contentRef.current) return;
 
     const parentWidth = containerRef.current.parentElement?.clientWidth;
-    if (!parentWidth) return;
+    if (!parentWidth) return console.warn("CampusInfoCard must have a parent element to calculate its expanded width.");
 
-    // Set absolute positioned content div to calculated target width
-    const currentWidth = containerRef.current?.clientWidth;
+    const currentWidth = contentWindowRef.current?.clientWidth;
     const targetWidth = parentWidth > maxWidth ? maxWidth : parentWidth;
+
     contentRef.current.style.width = `${targetWidth}px`;
 
     requestAnimationFrame(async () => {
-      if (!containerRef.current || !contentRef.current) return;
+      if (!contentRef.current) return;
 
-      setState("will-open");
-
-      // Measure the resulting content height after layout recalculation
+      const currentHeight = 0;
       const targetHeight = contentRef.current.clientHeight;
 
-      // Animate the container to the calculated width
-      const widthAnimation = containerRef.current!.animate(
-        [{ width: `${currentWidth}px` }, { width: `${targetWidth}px` }],
-        {
-          duration: 150,
-          easing: "ease",
-          iterations: 1,
-        },
-      );
+      const animationKeyframes: Keyframe[] = [
+        { width: `${currentWidth}px`, height: `${currentHeight}px` },
+        { width: `${targetWidth}px`, height: `${targetHeight}px` },
+      ];
 
-      // Animate the contentWindow to the measured height
-      const heightAnimation = contentWindowRef.current!.animate(
-        [{ height: `${0}px` }, { height: `${targetHeight}px` }],
-        {
-          duration: 150,
-          easing: "ease",
-          iterations: 1,
-        },
-      );
-
-      const widthAnimationPromise = new Promise((resolve) => {
-        widthAnimation.addEventListener("finish", resolve);
+      const animation = contentWindowRef.current!.animate(animationKeyframes, {
+        duration: animationDuration,
+        easing: "ease-out",
       });
 
-      const heightAnimationPromise = new Promise((resolve) => {
-        heightAnimation.addEventListener("finish", resolve);
+      setAnimationState("expanding");
+
+      animation.addEventListener("finish", () => {
+        if (!contentRef.current) return;
+        contentRef.current.style.position = "static";
+        setAnimationState("expanded");
       });
-
-      // Await both animations
-      await Promise.all([widthAnimationPromise, heightAnimationPromise]);
-
-      // Position the content ref statically, to prevent the content window from collapsing, after the animation was played
-      contentRef.current.style.position = "static";
-
-      // Finally set the open state to true, for other elements to react
-      setState("open");
     });
   };
 
-  const closeCard = () => {
-    if (!contentWindowRef.current || !contentRef.current) return;
+  const close = () => {
+    if (!contentRef.current) return;
     contentRef.current.style.position = "absolute";
-    setState("closed");
+    setAnimationState("closed");
   };
+
+  useEffect(() => {
+    if ((!active && animationState === "closed") || (active && animationState === "expanded")) return;
+    active ? expand() : close();
+  }, [active, animationState, expand, close]);
 
   return (
     <div
       ref={containerRef}
-      className={cn(
-        "group max-w-[500px] cursor-pointer rounded-[1.3rem] bg-neutral-100 transition-all duration-150 hover:rounded-2xl md:rounded-[4rem]",
-        { "w-fit": state === "closed", "!rounded-[1.3rem] pt-4": state === "will-open" || state === "open" },
-        className,
-      )}
+      className={cn("group w-fit cursor-pointer rounded-[1.3rem] bg-neutral-100 transition-all", className)}
+      style={{ maxWidth: `${maxWidth}px`, transitionDuration: `${animationDuration}ms` }}
       onClick={handleCardClick}
       {...restProps}
     >
-      <div className="flex items-center justify-between gap-4 p-4 pl-8">
-        <div>
+      <div className="flex items-start justify-between gap-4 p-4 pl-6">
+        <div className={cn("transition-[padding-top]", { "pt-4": active })}>
           <Heading size="sm" className="m-0 mt-1">
-            Title
+            {title}
           </Heading>
-          <Label className="text-neutral-100-text-muted">Subtitle</Label>
+          <Label className="text-neutral-100-text-muted">{subtitle}</Label>
         </div>
         <div
           className={cn(
             "relative h-8 w-8 overflow-hidden rounded-full bg-primary-900 text-primary-900-text transition-transform",
-            {
-              "rotate-180": state === "open",
-            },
+            { "rotate-180": active },
           )}
         >
           <div className="absolute left-0 top-0 grid h-full w-full place-items-center">
@@ -119,15 +130,32 @@ export const CampusInfoCard: FC<CampusInfoCardProps> = ({ className, onClick, ..
           </div>
         </div>
       </div>
-      <div ref={contentWindowRef} className={cn("relative overflow-hidden transition-[height]")}>
-        <div ref={contentRef} className="left-0 top-0 w-full px-8 pb-4" style={{ position: "absolute" }}>
-          <Paragraph>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam unde perspiciatis ipsam eos dolor itaque
-            iste. Possimus porro, minima aspernatur inventore illum corporis, quisquam suscipit nulla accusantium rerum
-            labore libero.
-          </Paragraph>
+      <div ref={contentWindowRef} className={cn("relative overflow-hidden")}>
+        <div ref={contentRef} className="left-0 top-0 px-6 pb-4" style={{ position: "absolute" }}>
+          {children}
         </div>
       </div>
     </div>
+  );
+};
+
+type CampusInfoCardListContext = { expandedID: string | null; setExpandedID: Dispatch<SetStateAction<string | null>> };
+const CampusInfoCardListContext = createContext<CampusInfoCardListContext>({
+  expandedID: null,
+  setExpandedID: () => {},
+});
+
+export const useCampusInfoCardList = () => {
+  const context = useContext(CampusInfoCardListContext);
+  if (!context) throw new Error("Hook useCampusInfoCardList must be used within a <CampusInfoCardListProvider>.");
+  return context;
+};
+
+export const CampusInfoCardListProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [expandedID, setExpandedID] = useState<CampusInfoCardListContext["expandedID"]>(null);
+  return (
+    <CampusInfoCardListContext.Provider value={{ expandedID, setExpandedID }}>
+      {children}
+    </CampusInfoCardListContext.Provider>
   );
 };
