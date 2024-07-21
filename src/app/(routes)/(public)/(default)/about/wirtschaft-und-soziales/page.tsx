0049e126a9222a30fa37e95ad6f0c0bbf13afc92
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-07-21 18:04:08 +0200

    Added placeholder text below economy social hero section

--- src/app/(routes)/(public)/(default)/about/wirtschaft-und-soziales/page.tsx
+++ src/app/(routes)/(public)/(default)/about/wirtschaft-und-soziales/page.tsx
@@ -3,6 +3,8 @@ import { FC } from "react";
 import { EconomyXSocialHero } from "~/app/_components/compounds/economy-x-social-hero";
 import { sanity } from "~/sanity/lib/client";
 import { EconomySocialPageQueryResult } from "../../../../../../../generated/sanity/types";
+import { Container } from "~/app/_components/primitives/container";
+import { Paragraph } from "~/app/_components/primitives/typography";
 
 const economySocialPageQuery = groq`*[_type == "economy-social-page"][0]`;
 
@@ -14,11 +16,20 @@ const EconomyXSocialPage: FC = async () => {
   );
 
   return (
-    <EconomyXSocialHero
+    <>
+      <EconomyXSocialHero
         headingUpper={data?.headingUpper || ""}
         headingLower={data?.headingLower || ""}
         description={data?.previewText || ""}
       />
+      <Container width="narrow" className="my-32">
+        <Paragraph>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, dolorem. Itaque quae quo illum
+          perspiciatis nam ad quod quas, optio omnis autem explicabo totam accusantium esse ut nesciunt architecto
+          earum.
+        </Paragraph>
+      </Container>
+    </>
   );
 };
 
