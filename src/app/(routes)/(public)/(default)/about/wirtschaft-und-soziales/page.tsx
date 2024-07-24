ee1b5ca61639a37e29cb7488df61d5837ac94b31
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-07-24 17:24:34 +0200

    Added a BlockContent component to the economy social page

--- src/app/(routes)/(public)/(default)/about/wirtschaft-und-soziales/page.tsx
+++ src/app/(routes)/(public)/(default)/about/wirtschaft-und-soziales/page.tsx
@@ -4,9 +4,42 @@ import { EconomyXSocialHero } from "~/app/_components/compounds/economy-x-social
 import { sanity } from "~/sanity/lib/client";
 import { EconomySocialPageQueryResult } from "../../../../../../../generated/sanity/types";
 import { Container } from "~/app/_components/primitives/container";
-import { Paragraph } from "~/app/_components/primitives/typography";
+import { BlockContent } from "~/app/_components/compounds/block-content";
 
-const economySocialPageQuery = groq`*[_type == "economy-social-page"][0]`;
+const economySocialPageQuery = groq`*[_type == "economy-social-page"][0] {
+  ...,
+  content[] {
+    ...,
+    _type == "image" => {
+      ...,
+      asset -> { url }
+    },
+    _type == "educationalProgramTypeCTA" => {
+      ...,
+      educationalProgramType -> {
+        name,
+        promotionalHeadline,
+        color,
+        slug,
+        readMoreLabel
+      }
+    },
+    _type == "educationalProgramCTA" => {
+      ...,
+      educationalProgram -> {
+        name,
+        promotionalHeadline,
+        color,
+        slug,
+        readMoreLabel,
+        educationalProgramType -> {
+          name,
+          color
+        }
+      }
+    }
+  }
+}`;
 
 const EconomyXSocialPage: FC = async () => {
   const data = await sanity.fetch<EconomySocialPageQueryResult>(
@@ -23,11 +56,7 @@ const EconomyXSocialPage: FC = async () => {
         description={data?.previewText || ""}
       />
       <Container width="narrow" className="my-32">
-        <Paragraph>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, dolorem. Itaque quae quo illum
-          perspiciatis nam ad quod quas, optio omnis autem explicabo totam accusantium esse ut nesciunt architecto
-          earum.
-        </Paragraph>
+        <BlockContent blockContent={data?.content || []} />
       </Container>
     </>
   );
