a9519746389ccf3781c007b78da3f36167c23c57
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-07-28 19:50:30 +0200

    Added basic cookie banner

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -6,6 +6,8 @@ import { catamaran, tiempos } from "~/app/_fonts/fonts";
 import "~/app/_styles/globals.css";
 import "~/app/_styles/theme.css";
 import { QueryProvider } from "../_components/primitives/query-provider";
+import { Container } from "../_components/primitives/container";
+import { CookieNotice } from "../_components/compounds/cookie-notice/cookie-notice";
 
 export const metadata: Metadata = {
   title: "Emil Molt Akademie",
@@ -25,6 +27,11 @@ const PublicLayout: FC<PropsWithChildren> = ({ children }) => {
           )}
         >
           {children}
+          <div className="fixed bottom-0 left-0 w-full">
+            <Container className="flex justify-end">
+              <CookieNotice className="mb-2 lg:mb-8" />
+            </Container>
+          </div>
         </body>
       </html>
     </QueryProvider>
