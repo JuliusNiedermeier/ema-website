4bc7b8ea83f601335508c9bb39d93e65b25f9e73
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-07-29 11:01:06 +0200

    Removed scroll based entrance of cookie notice

--- src/app/_components/compounds/cookie-notice/cookie-notice.tsx
+++ src/app/_components/compounds/cookie-notice/cookie-notice.tsx
@@ -1,48 +1,33 @@
-"use client";
-
 import { ComponentProps, FC } from "react";
 import { cn } from "~/app/_utils/cn";
 import { Card } from "../../primitives/card";
 import { Label } from "../../primitives/typography";
 import { Button } from "../../primitives/button";
 import { InteractionBubble } from "../interaction-bubble";
-import { motion, useScroll, useTransform } from "framer-motion";
 
 export type CookieNoticeProps = Omit<ComponentProps<typeof Card>, "children"> & {};
 
 export const CookieNotice: FC<CookieNoticeProps> = ({ className, ...restProps }) => {
-  const { scrollY } = useScroll();
-
-  const progress = useTransform(scrollY, [0, 500, 800], [100, 100, 0]);
-  const transformY = useTransform(progress, (value) => `${value}%`);
-  const opacity = useTransform(progress, [100, 0], [0, 1]);
-
   return (
-    <Card
-      className={cn("w-fit max-w-96 border border-neutral-400/20 bg-primary-900", className)}
-      {...restProps}
-      asChild
-    >
-      <motion.div style={{ y: transformY, opacity }}>
-        <Label className="text-neutral-900-text">Wir nutzen Cookies</Label>
-        <Label className="mt-2 block text-neutral-900-text-muted">
-          Wenn Sie auf „Akzeptieren“ klicken, erklären Sie sich mit der Speicherung von Cookies auf Ihrem Gerät
-          einverstanden.
-        </Label>
-        <div className="mt-4 flex gap-2">
-          <Button
-            size="sm"
-            vairant="outline"
-            className="text-neutral-900-text-muted transition-colors hover:text-neutral-900-text"
-          >
-            <Label>Ablehnen</Label>
-          </Button>
-          <Button size="sm" vairant="outline" className="flex-1 bg-primary-100 pr-1 text-primary-100-text">
-            <Label className="block flex-1">Akzeptieren</Label>
-            <InteractionBubble animated={false} />
-          </Button>
-        </div>
-      </motion.div>
+    <Card className={cn("w-fit max-w-96 border border-neutral-400/20 bg-primary-900", className)} {...restProps}>
+      <Label className="text-neutral-900-text">Wir nutzen Cookies</Label>
+      <Label className="mt-2 block text-neutral-900-text-muted">
+        Wenn Sie auf „Akzeptieren“ klicken, erklären Sie sich mit der Speicherung von Cookies auf Ihrem Gerät
+        einverstanden.
+      </Label>
+      <div className="mt-4 flex gap-2">
+        <Button
+          size="sm"
+          vairant="outline"
+          className="text-neutral-900-text-muted transition-colors hover:text-neutral-900-text"
+        >
+          <Label>Ablehnen</Label>
+        </Button>
+        <Button size="sm" vairant="outline" className="flex-1 bg-primary-100 pr-1 text-primary-100-text">
+          <Label className="block flex-1">Akzeptieren</Label>
+          <InteractionBubble animated={false} />
+        </Button>
+      </div>
     </Card>
   );
 };
