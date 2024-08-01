ddae23ab0f35eae92dbdf5f781268ecaa36d8312
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-08-01 11:20:34 +0200

    Created sanityFetch function that simplifies fetching from sanity and handles fetching in draft mode

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,33 @@
-import { createClient } from "next-sanity";
+import "server-only";
+
+import { createClient, QueryOptions, QueryParams } from "next-sanity";
 import { env } from "~/env";
 import { apiVersion } from "~/sanity/config";
+import { draftMode } from "next/headers";
 
 export const sanity = createClient({
   apiVersion,
   dataset: env.NEXT_PUBLIC_SANITY_DATASET,
   projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   useCdn: false,
+  stega: {
+    enabled: process.env.NEXT_PUBLIC_VERCEL_ENV === "preview",
+    studioUrl: "/studio",
+  },
 });
+
+export const sanityFetch = async <QueryResponse>(query: string, tags: string[], params: QueryParams = {}) => {
+  const isDraftMode = draftMode().isEnabled;
+
+  const queryOptions: QueryOptions = {
+    token: isDraftMode ? env.SANITY_READ_TOKEN : undefined,
+    perspective: isDraftMode ? "previewDrafts" : undefined,
+    stega: isDraftMode,
+    next: {
+      revalidate: isDraftMode ? 0 : undefined, // Do not cache if in draft mode
+      tags,
+    },
+  };
+
+  return sanity.fetch<QueryResponse>(query, params, queryOptions);
+};
