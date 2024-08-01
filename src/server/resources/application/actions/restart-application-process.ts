b0bb3dcf9112fa80eb39d7d01e1cb77ba0190ed4
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-08-01 14:38:56 +0200

    Made restartApplicationProcess an async function to prevent error annotation

--- src/server/resources/application/actions/restart-application-process.ts
+++ src/server/resources/application/actions/restart-application-process.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 import { applicationCookieName } from "../application-cookie";
 import { redirect } from "next/navigation";
 
-export const restartApplicationProcess = () => {
+export const restartApplicationProcess = async () => {
   cookies().delete(applicationCookieName);
   redirect("/go");
 };
