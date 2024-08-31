12ca12ee752c5f40c6923a35f2c2e01643fc206b
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-08-31 12:26:17 +0200

    Created first draft of the overview/comparison page

---
+++ src/app/(routes)/(public)/(default)/ubersicht/page.tsx
@@ -0,0 +1,70 @@
+import { CheckIcon, MinusIcon } from "lucide-react";
+import { FC, Fragment } from "react";
+import { Container } from "~/app/_components/primitives/container";
+import { Heading, Label, Paragraph } from "~/app/_components/primitives/typography";
+import { cn } from "~/app/_utils/cn";
+
+const xCount = 7;
+const yCount = 15;
+
+const ÜbersichtPage: FC = () => {
+  return (
+    <div className="bg-neutral-200 py-32">
+      <Container width="narrow" className="text-center">
+        <Heading>Alle Bildungsgänge im Überblick</Heading>
+        <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque ipsum similique nihil hic quidem non ad, ex
+          nisi natus nostrum earum dolorem, unde, distinctio placeat dolor vero obcaecati quas labore!
+        </Paragraph>
+      </Container>
+      <Container className="mt-32">
+        <div
+          className="relative grid overflow-x-auto overflow-y-hidden rounded-3xl border"
+          style={{
+            gridTemplateColumns: `repeat(${xCount},min-content)`,
+            // gridTemplateRows: `repeat(${yCount},fit-content)`,
+          }}
+        >
+          {Array.from(new Array(yCount)).map((_, yIndex) =>
+            Array.from(new Array(xCount)).map((_, xIndex) => {
+              const checked = Boolean(Math.round(Math.random()));
+              return (
+                <Fragment key={`${yIndex}-${xIndex}`}>
+                  {yIndex === 0 && xIndex === 0 ? (
+                    <div className="bg-[#e38264]" />
+                  ) : yIndex === 0 ? (
+                    <div
+                      className={cn("bg-[#e38264] p-4", {
+                        "border-l border-neutral-100-text-muted": xIndex > 0,
+                      })}
+                    >
+                      <Label className="block text-neutral-100-text-muted">Fachschule</Label>
+                      <Label>Heilerziehungspflege</Label>
+                    </div>
+                  ) : xIndex === 0 ? (
+                    <div className="sticky left-0 max-w-[10vw] overflow-hidden border-t border-primary-900 bg-primary-900 p-2">
+                      <Label className="text-neutral-900-text" style={{ overflowWrap: "break-word" }}>
+                        Politikwissenschaften
+                      </Label>
+                    </div>
+                  ) : (
+                    <div
+                      className={cn("wrap grid place-content-center border-l border-t border-neutral-400 p-2", {
+                        "bg-primary-900/40 text-primary-900": checked,
+                        "text-neutral-400": !checked,
+                      })}
+                    >
+                      {checked ? <CheckIcon /> : <MinusIcon />}
+                    </div>
+                  )}
+                </Fragment>
+              );
+            }),
+          )}
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default ÜbersichtPage;
