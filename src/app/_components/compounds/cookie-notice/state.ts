d47418cbc00cd36140254ea5f2322c6affd71337
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-09-01 18:49:19 +0200

    Fixed the cookie banner, always showing up on page load

--- src/app/_components/compounds/cookie-notice/state.ts
+++ src/app/_components/compounds/cookie-notice/state.ts
@@ -1,21 +1,26 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-type CookieConsentStatus = "awaiting" | "rejected" | "accepted";
+type CookieConsentStatus = null | "awaiting" | "rejected" | "accepted";
 
 export type CookieConsentState = {
   consent: CookieConsentStatus;
-  setConsent: (consent: CookieConsentStatus) => void;
+  setConsent: (consent: Exclude<CookieConsentStatus, null>) => void;
 };
 
 export const useCookieConsentState = create(
   persist<CookieConsentState>(
     (set, get) => ({
-      consent: "awaiting",
+      consent: null,
       setConsent: (consent) => set((state) => ({ ...state, consent })),
     }),
     {
       name: "cookie-consent",
+      onRehydrateStorage: () => {
+        // Appearently setters must be called in this returned callback and on the state passed to it.
+        // Not the state passed to onRehydrateStorage
+        return (state) => state?.consent === null && state.setConsent("awaiting");
+      },
     },
   ),
 );
