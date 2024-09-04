d9268d9f74eb9252acb82cff31cd4ebded2be4f3
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-09-04 15:42:49 +0200

    Created fees chart on fees page

--- src/app/(routes)/(public)/(default)/schulbeitrag/page.tsx
+++ src/app/(routes)/(public)/(default)/schulbeitrag/page.tsx
@@ -1,16 +1,73 @@
-import { FC } from "react";
+import { groq } from "next-sanity";
+import { notFound } from "next/navigation";
+import { ComponentProps, FC } from "react";
 import { Container } from "~/app/_components/primitives/container";
 import { Heading, Paragraph } from "~/app/_components/primitives/typography";
+import { sanityFetch } from "~/sanity/lib/client";
+import { FeesPageProgramsQueryResult, FeesPageQueryResult } from "../../../../../../generated/sanity/types";
+import { ProgramPricingChart } from "~/app/_components/compounds/program-pricing-chart";
+import { ensureValidHSL } from "~/app/_utils/color-swatch";
+
+const feesPageQuery = groq`*[_type == "fees-page"][0] {
+  ...
+}`;
+
+const feesPageProgramsQuery = groq`*[_type == "educational-program"] {
+  _id,
+  name,
+  educationalProgramType -> {
+    _id,
+    name,
+    color
+  }
+}`;
 
 const FeesPage: FC = async () => {
+  const pageData = await sanityFetch<FeesPageQueryResult>(feesPageQuery, { tags: ["fees-page"] });
+
+  if (!pageData) notFound();
+
+  const programs = await sanityFetch<FeesPageProgramsQueryResult>(feesPageProgramsQuery, {
+    tags: ["educational-program", "educational-program-type"],
+  });
+
+  const programFees: ComponentProps<typeof ProgramPricingChart>["programFees"] = programs
+    ?.filter((program) => program.educationalProgramType)
+    .map((program) => ({
+      program: {
+        ID: program._id,
+        title: program.name || "",
+        type: { ID: program.educationalProgramType!._id, title: program.educationalProgramType?.name || "" },
+        color: ensureValidHSL(program.educationalProgramType?.color?.hsl),
+      },
+      fees: [
+        { incomeRangeLabel: "unter 19k €", fee: 75 },
+        { incomeRangeLabel: "19k bis 22k €", fee: 80 },
+        { incomeRangeLabel: "22k bis 24k €", fee: 90 },
+        { incomeRangeLabel: "24k bis 26k €", fee: 100 },
+        { incomeRangeLabel: "26k bis 28k €", fee: 115 },
+        { incomeRangeLabel: "28k bis 32k €", fee: 125 },
+        { incomeRangeLabel: "32k bis 35k €", fee: 130 },
+        { incomeRangeLabel: "35k bis 38k €", fee: 135 },
+        { incomeRangeLabel: "38k bis 40k €", fee: 145 },
+        { incomeRangeLabel: "40k bis 42k €", fee: 155 },
+        { incomeRangeLabel: "42k bis 44k €", fee: 165 },
+        { incomeRangeLabel: "44k bis 48k €", fee: 175 },
+        { incomeRangeLabel: "48k bis 52k €", fee: 190 },
+        { incomeRangeLabel: "52k bis 56k €", fee: 200 },
+        { incomeRangeLabel: "über 56k €", fee: 220 },
+      ],
+    }));
+
   return (
     <Container width="narrow" className="my-32">
-      <Heading>Schulbeitrag</Heading>
+      <Heading>{pageData.heading}</Heading>
       <Paragraph>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aliquid consectetur, doloremque architecto
         aliquam quae. Minus, porro laudantium? Facere tempora perspiciatis minima impedit illo ducimus fugit totam
         distinctio atque expedita?
       </Paragraph>
+      <ProgramPricingChart className="mt-12" programFees={programFees} />
     </Container>
   );
 };
---
+++ src/app/_components/compounds/program-pricing-chart.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { ComponentProps, FC, useMemo, useState } from "react";
+import { transform } from "framer-motion";
+import * as Select from "@radix-ui/react-select";
+import { CheckIcon, ChevronDownIcon, ChevronsUpDownIcon, ChevronUpIcon } from "lucide-react";
+import { createColorThemeStyles, HSLValue } from "~/app/_utils/color-swatch";
+import { cn } from "~/app/_utils/cn";
+import { Label } from "../primitives/typography";
+import { groupBy } from "~/app/_utils/group-by";
+import { Chip } from "../primitives/chip";
+
+export type ProgramPricingChartProps = ComponentProps<"div"> & {
+  programFees: {
+    program: { ID: string; title: string; type: { ID: string; title: string }; color: HSLValue };
+    fees: { incomeRangeLabel: string; fee: number }[];
+  }[];
+};
+
+export const ProgramPricingChart: FC<ProgramPricingChartProps> = ({ className, programFees, ...restProps }) => {
+  const [programID, setProgramID] = useState<string>("");
+
+  const selectedProgram = useMemo(() => {
+    const selectedProgram = programFees.find(({ program }) => program.ID === programID);
+
+    if (!selectedProgram) return { program: null, fees: [] };
+
+    const feeValues = selectedProgram.fees.map((fee) => fee.fee);
+    const minFee = Math.min(...feeValues);
+    const maxFee = Math.max(...feeValues);
+
+    const mappedFees = selectedProgram.fees.map((fee) => ({
+      ...fee,
+      feePercentage: transform(fee.fee, [minFee, maxFee], [75, 100]),
+    }));
+
+    return { program: selectedProgram.program, fees: mappedFees };
+  }, [programFees, programID]);
+
+  const groupedPrograms = groupBy(programFees, (program) => program.program.type.ID);
+
+  return (
+    <div className={cn("", className)} {...restProps}>
+      {/* PROGRAM SELECT */}
+      <Chip className="border border-neutral-400 bg-neutral-200">
+        <Label>Ausgewählter Bildungsgang</Label>
+      </Chip>
+      <Select.Root value={programID} onValueChange={setProgramID}>
+        <Select.Trigger className="relative mt-2 w-full rounded-2xl">
+          <Select.Value placeholder="Bildungsgang auswählen" />
+          <Select.Icon className="absolute bottom-0 right-0 top-0 grid w-12 place-items-center">
+            <ChevronsUpDownIcon size={18} />
+          </Select.Icon>
+        </Select.Trigger>
+        <Select.Portal>
+          <Select.Content className="z-50 overflow-hidden rounded-3xl border border-neutral-400 bg-neutral-100 shadow">
+            <Select.ScrollUpButton className="flex h-[25px] cursor-default items-center justify-center bg-neutral-100">
+              <ChevronUpIcon />
+            </Select.ScrollUpButton>
+            <Select.Viewport className="grid gap-2 p-2">
+              {Object.keys(groupedPrograms).map((programID) => {
+                const colorThemeStyles = createColorThemeStyles(groupedPrograms[programID][0].program.color);
+                return (
+                  <Select.Group key={programID} className="rounded-2xl bg-themed-primary p-1" style={colorThemeStyles}>
+                    {groupedPrograms[programID].map(({ program, fees }) => (
+                      <Select.Item
+                        key={program.ID}
+                        value={program.ID}
+                        className="relative data-[highlighted]:outline-none [&[data-highlighted]_[data-program-item]]:bg-themed-secondary"
+                      >
+                        <Select.ItemText>
+                          <div
+                            data-program-item
+                            className="rounded-xl bg-themed-primary p-3 pr-14 text-left leading-none"
+                            style={colorThemeStyles}
+                          >
+                            <Label className="text-neutral-100-text-muted">{program.type.title}</Label>
+                            <Label className="mt-1 block">{program.title}</Label>
+                          </div>
+                        </Select.ItemText>
+                        <Select.ItemIndicator className="absolute bottom-0 right-0 top-0 grid w-12 place-items-center">
+                          <CheckIcon size={18} />
+                        </Select.ItemIndicator>
+                      </Select.Item>
+                    ))}
+                  </Select.Group>
+                );
+              })}
+            </Select.Viewport>
+            <Select.ScrollDownButton className="flex h-[25px] cursor-default items-center justify-center bg-neutral-100">
+              <ChevronDownIcon />
+            </Select.ScrollDownButton>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+
+      {/* FEE CHART */}
+      <div className="mt-8 flex items-center justify-between rounded-full border border-neutral-400 bg-neutral-200 p-2">
+        <Chip className="border border-neutral-400 bg-neutral-300">
+          <Label>Haushaltseinkommen</Label>
+        </Chip>
+        <Label className="mr-4 block">Beitrag</Label>
+      </div>
+      <div className="mt-2 flex flex-col gap-2">
+        {selectedProgram.fees.toReversed().map((fee, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-between rounded-full bg-primary-900 p-2 hover:bg-primary-900/90"
+            style={{ width: `${fee.feePercentage}%` }}
+          >
+            <Chip>
+              <Label>{fee.incomeRangeLabel}</Label>
+            </Chip>
+            <Label className="mr-4 text-neutral-900-text">{fee.fee} €</Label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
