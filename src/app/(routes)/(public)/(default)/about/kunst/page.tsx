a889fabf1eb0ae9c720e952a244f8dbdf7de9f90
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-09-05 13:07:28 +0200

    Replaced custom art page gallery with generic ParalaxGallery

--- src/app/(routes)/(public)/(default)/about/kunst/page.tsx
+++ src/app/(routes)/(public)/(default)/about/kunst/page.tsx
@@ -1,6 +1,5 @@
 import { groq } from "next-sanity";
-import Image from "next/image";
-import { FC } from "react";
+import { ComponentProps, FC } from "react";
 import { Container } from "~/app/_components/primitives/container";
 import { Heading, Paragraph } from "~/app/_components/primitives/typography";
 import { sanityFetch } from "~/sanity/lib/client";
@@ -8,6 +7,7 @@ import { ArtPageQueryResult } from "../../../../../../../generated/sanity/types"
 import { notFound } from "next/navigation";
 import { EducationalProgramTypeCards } from "~/app/_components/blocks/educational-program-type-cards";
 import { EndOfPageCTA } from "~/app/_components/compounds/end-of-page-cta";
+import { ParalaxGallery } from "~/app/_components/compounds/paralax-gallery";
 
 const artPageQuery = groq`*[_type == "art-page"][0]{
   ...,
@@ -23,50 +23,25 @@ const ArtPage: FC = async () => {
 
   if (!data) notFound();
 
+  const galleryItems: ComponentProps<typeof ParalaxGallery>["items"] =
+    data?.artSubjects?.map((subject) => ({
+      imageURL: subject.image?.asset?.url || "",
+      heading: subject.title || "",
+      subheading: subject.slogan || "",
+      description: subject.description || "",
+    })) || [];
+
   return (
     <>
-      <div className="absolute left-0 top-0 -z-10 h-[300vh] w-full bg-neutral-100">
-        <div className="relative h-full">
-          <Image
-            src={data.background?.asset?.url || ""}
-            width="1920"
-            height="1080"
-            alt={data.heading || ""}
-            className="h-screen overflow-hidden object-cover"
-          />
-          <div className="absolute left-0 top-0 h-full w-full bg-neutral-200/80 backdrop-blur-3xl"></div>
-          <div className="absolute left-0 top-[200vh] h-screen w-full bg-gradient-to-b from-neutral-200 to-neutral-100"></div>
-        </div>
+      <div className="bg-neutral-200 pb-40">
+        <Container width="narrow" className="py-32 text-center">
+          <Heading>{data.heading}</Heading>
+          <Paragraph>{data.preview?.excerpt}</Paragraph>
+        </Container>
       </div>
-      <Container width="narrow" className="pt-32 text-center">
-        <Heading>{data.heading}</Heading>
-        <Paragraph>{data.preview?.excerpt}</Paragraph>
-      </Container>
-      <Container className="mt-32 [&>*+*]:mt-12">
-        {data.artSubjects?.map((subject, index) => (
-          <div
-            key={index}
-            className="flex flex-col rounded-3xl border border-neutral-400 bg-neutral-100 p-4 md:flex-row md:items-start md:gap-12"
-          >
-            <div className="top-24 py-4 md:sticky md:flex-1 md:px-4">
-              <Heading size="sm" className="m-0">
-                {subject.title}
-              </Heading>
-              <Paragraph className="mt-1">{subject.slogan}</Paragraph>
-            </div>
-            <div className="ml-auto max-w-[60rem] md:flex-[2]">
-              <Image
-                src={subject.image?.asset?.url || ""}
-                alt={subject.title || ""}
-                width="1920"
-                height="1080"
-                className="aspect-video w-full rounded-2xl object-cover"
-              />
-              <Paragraph className="max-w-[40rem]x mt-12">{subject.description}</Paragraph>
-            </div>
-          </div>
-        ))}
-      </Container>
+
+      <ParalaxGallery items={galleryItems} className="-mt-40" />
+
       <Container className="mt-48">
         <EndOfPageCTA
           heading={data.educationalProgramTypesCTA?.heading || ""}
