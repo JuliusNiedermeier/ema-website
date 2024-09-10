b3289347f71fc3d2d1447912835e23609ac0ab90
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-09-10 11:26:35 +0200

    Improved appointment-request-form

--- src/app/_components/compounds/appointment-request-form.tsx
+++ src/app/_components/compounds/appointment-request-form.tsx
@@ -40,19 +40,26 @@ export const AppointmentRequestForm: FC<AppointmentRequestFormProps> = ({
     },
   });
 
+  const showSuccess = submitAppointmentRequest.isSuccess;
+
   return (
     <div className={cn("", className)} {...restProps}>
       <div
-        className={cn("grid grid-rows-[0fr] overflow-hidden opacity-0 transition-all [&>*]:min-h-0", {
-          "mb-8 grid-rows-[1fr] opacity-100": submitAppointmentRequest.isSuccess,
+        className={cn("grid translate-y-10 grid-rows-[0fr] overflow-hidden opacity-0 transition-all [&>*]:min-h-0", {
+          "grid-rows-[1fr] opacity-100": showSuccess,
         })}
       >
         <div>
-          <div className="flex flex-col gap-2 rounded-2xl bg-primary-900 p-4 text-neutral-900-text sm:flex-row sm:items-center sm:gap-4 sm:p-2">
+          <div
+            className={cn(
+              "flex flex-col gap-2 rounded-2xl bg-primary-900 p-4 text-neutral-900-text sm:flex-row sm:items-center sm:gap-4 sm:p-2",
+              { "pb-14 sm:pb-14": showSuccess },
+            )}
+          >
             <div className="flex items-center gap-3 rounded-xl bg-primary-100 p-3 text-primary-100-text">
               <SendHorizonalIcon
                 className={cn("-translate-x-4 opacity-0 transition-all delay-300", {
-                  "translate-x-0 opacity-100": submitAppointmentRequest.isSuccess,
+                  "translate-x-0 opacity-100": showSuccess,
                 })}
               />
               <Label>{successLabel}</Label>
@@ -61,16 +68,17 @@ export const AppointmentRequestForm: FC<AppointmentRequestFormProps> = ({
           </div>
         </div>
       </div>
-      <div className="flex flex-col gap-4 sm:flex-row">
+      <div className="relative z-10 flex flex-col gap-2 border-primary-900 ring-neutral-100 sm:flex-row sm:gap-0 sm:rounded-full sm:border-2 sm:ring">
         <input
-          className="block flex-1 rounded-2xl border bg-transparent p-4 font-serif text-neutral-100-text outline-none"
+          className="block flex-1 rounded-full border-2 border-primary-900 bg-primary-100 px-8 py-4 text-center font-serif text-neutral-100-text placeholder-neutral-100-text-muted outline-none ring ring-neutral-100 sm:rounded-r-none sm:border-none sm:text-left sm:ring-0"
+          autoFocus
           type="email"
           placeholder={emailPlaceholder}
           value={email}
           onInput={(e) => setEmail(e.currentTarget.value)}
         />
         <Button
-          className="w-full justify-center sm:w-fit"
+          className="w-full justify-center sm:-ml-8 sm:w-fit"
           onClick={() => submitAppointmentRequest.mutate({ email })}
           disabled={!valid}
         >
@@ -79,14 +87,14 @@ export const AppointmentRequestForm: FC<AppointmentRequestFormProps> = ({
           ) : (
             <>
               <Label>{submitButtonLabel}</Label>
-              <ButtonInteractionBubble />
+              <ButtonInteractionBubble className="bg-primary-100 text-primary-100-text" />
             </>
           )}
         </Button>
       </div>
       <Turnstile
         onVerify={(token) => setTurnstileToken(token)}
-        className="mx-auto mt-8 rounded-2xl mix-blend-darken"
+        className="mx-auto mt-4 rounded-2xl mix-blend-darken"
         appearance="interaction-only"
       />
     </div>
