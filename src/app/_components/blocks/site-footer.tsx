ec74a8a189bfe07d4c90af85d048774384b48b37
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-09-13 16:22:36 +0200

    Added padding around site-footer width solid neutral-100 background color

--- src/app/_components/blocks/site-footer.tsx
+++ src/app/_components/blocks/site-footer.tsx
@@ -66,109 +66,108 @@ export const SiteFooter: FC<SiteFooterProps> = async ({ className, ...restProps 
   ];
 
   return (
-    <div
-      className={cn("mt-8 rounded-t-3xl border-b-0 bg-primary-900 sm:m-2 sm:mt-8 sm:rounded-b-3xl", className)}
-      {...restProps}
-    >
-      <Container asChild>
-        <footer className="p-4 sm:p-0">
-          <div className="flex flex-col justify-between gap-12 pt-28 sm:gap-40 lg:flex-row">
-            <div>
-              <SiteLogo show="mark" variant="light" />
-              <SiteLogo show="text" variant="light" className="mt-4" />
-              <Paragraph className="mt-8 text-neutral-900-text">
-                {footerConfig?.ctaHeading?.split(".").map((part, index, parts) => (
-                  <>
-                    {part}
-                    {index < parts.length - 1 && (
-                      <>
-                        .<br />
-                      </>
-                    )}
-                  </>
-                ))}
-              </Paragraph>
-              <Button
-                href="/go"
-                className="mt-8 w-full gap-4 bg-primary-100 pl-4 text-primary-100-text sm:w-fit"
-                vairant="outline"
-              >
-                <InteractionBubble animated={false} />
-                <Label>{footerConfig?.cta}</Label>
-              </Button>
+    <div className={cn("bg-neutral-100 pt-8 sm:p-2 sm:pt-8", className)} {...restProps}>
+      <div className="rounded-t-3xl bg-primary-900 sm:rounded-b-3xl">
+        <Container asChild>
+          <footer className="">
+            <div className="flex flex-col justify-between gap-12 pt-28 sm:gap-40 lg:flex-row">
+              <div>
+                <SiteLogo show="mark" variant="light" />
+                <SiteLogo show="text" variant="light" className="mt-4" />
+                <Paragraph className="mt-8 text-neutral-900-text">
+                  {footerConfig?.ctaHeading?.split(".").map((part, index, parts) => (
+                    <>
+                      {part}
+                      {index < parts.length - 1 && (
+                        <>
+                          .<br />
+                        </>
+                      )}
+                    </>
+                  ))}
+                </Paragraph>
+                <Button
+                  href="/go"
+                  className="mt-8 w-full gap-4 bg-primary-100 pl-4 text-primary-100-text sm:w-fit"
+                  vairant="outline"
+                >
+                  <InteractionBubble animated={false} />
+                  <Label>{footerConfig?.cta}</Label>
+                </Button>
+              </div>
+              <div className="grid grid-cols-1 gap-16 sm:grid-cols-2">
+                <List>
+                  <ListHeading>{footerConfig?.linkSections?.academy?.heading}</ListHeading>
+                  <ListContent>
+                    {academyLinkURLList.map(([key, URL], index) => (
+                      <Link key={index} href={URL}>
+                        <ListContentItem>{academyLinkLabelMap[key]}</ListContentItem>
+                      </Link>
+                    ))}
+                  </ListContent>
+                </List>
+                <List>
+                  <ListHeading>{footerConfig?.linkSections?.programs?.heading}</ListHeading>
+                  <ListContent>
+                    {programTypesWithPrograms.map((programType) => (
+                      <Fragment key={programType._id}>
+                        <Link href={`/bildungswege/${programType.slug?.current}`}>
+                          <ListContentItem>{programType.name}</ListContentItem>
+                        </Link>
+                        <ListContentItem>
+                          <ListContent className="mb-4">
+                            {programType.programs.map((program) => (
+                              <ListContentItem key={program._id} className="text-neutral-900-text-muted">
+                                <Link href={`/bildungswege/${programType.slug?.current}/${program.slug?.current}`}>
+                                  {program.name}
+                                </Link>
+                              </ListContentItem>
+                            ))}
+                          </ListContent>
+                        </ListContentItem>
+                      </Fragment>
+                    ))}
+                  </ListContent>
+                </List>
+              </div>
             </div>
-            <div className="grid grid-cols-1 gap-16 sm:grid-cols-2">
-              <List>
-                <ListHeading>{footerConfig?.linkSections?.academy?.heading}</ListHeading>
-                <ListContent>
-                  {academyLinkURLList.map(([key, URL], index) => (
-                    <Link key={index} href={URL}>
-                      <ListContentItem>{academyLinkLabelMap[key]}</ListContentItem>
+            <div className="mt-16 h-px bg-neutral-900-text-muted" />
+            <div className="flex flex-col-reverse justify-between gap-4 py-8 text-neutral-900-text sm:items-stretch lg:flex-row lg:items-center lg:gap-12">
+              <Label className="text-balance text-[0.9rem] text-neutral-900-text-muted">
+                {footerConfig?.copyrightNotice}
+              </Label>
+
+              <div className="flex flex-col-reverse gap-4 sm:flex-row lg:items-center">
+                <div className="flex items-center gap-4 text-neutral-900-text-muted">
+                  <Link href="/datenschutz">
+                    <Label>{footerConfig?.legalLinks?.privacy}</Label>
+                  </Link>
+                  <Link href="/impressum">
+                    <Label>{footerConfig?.legalLinks?.impressum}</Label>
+                  </Link>
+                </div>
+
+                <ResetCookieConsent label="Cookie-Einstellungen" />
+
+                <div className="mb-8 flex items-center gap-4 sm:mb-0 sm:ml-auto lg:ml-0">
+                  {footerConfig?.socialLinks?.map((link, index) => (
+                    <Link key={index} href={link.url || ""} className="h-6 w-6">
+                      <Image
+                        key={index}
+                        src={link.logoIcon?.asset?.url || ""}
+                        width={100}
+                        height={100}
+                        alt={link.platformName || ""}
+                        className="h-full w-full object-contain"
+                      />
                     </Link>
                   ))}
-                </ListContent>
-              </List>
-              <List>
-                <ListHeading>{footerConfig?.linkSections?.programs?.heading}</ListHeading>
-                <ListContent>
-                  {programTypesWithPrograms.map((programType) => (
-                    <Fragment key={programType._id}>
-                      <Link href={`/bildungswege/${programType.slug?.current}`}>
-                        <ListContentItem>{programType.name}</ListContentItem>
-                      </Link>
-                      <ListContentItem>
-                        <ListContent className="mb-4">
-                          {programType.programs.map((program) => (
-                            <ListContentItem key={program._id} className="text-neutral-900-text-muted">
-                              <Link href={`/bildungswege/${programType.slug?.current}/${program.slug?.current}`}>
-                                {program.name}
-                              </Link>
-                            </ListContentItem>
-                          ))}
-                        </ListContent>
-                      </ListContentItem>
-                    </Fragment>
-                  ))}
-                </ListContent>
-              </List>
-            </div>
-          </div>
-          <div className="mt-16 h-px bg-neutral-900-text-muted" />
-          <div className="flex flex-col-reverse justify-between gap-4 py-8 text-neutral-900-text sm:items-stretch lg:flex-row lg:items-center lg:gap-12">
-            <Label className="text-balance text-[0.9rem] text-neutral-900-text-muted">
-              {footerConfig?.copyrightNotice}
-            </Label>
-
-            <div className="flex flex-col-reverse gap-4 sm:flex-row lg:items-center">
-              <div className="flex items-center gap-4 text-neutral-900-text-muted">
-                <Link href="/datenschutz">
-                  <Label>{footerConfig?.legalLinks?.privacy}</Label>
-                </Link>
-                <Link href="/impressum">
-                  <Label>{footerConfig?.legalLinks?.impressum}</Label>
-                </Link>
-              </div>
-
-              <ResetCookieConsent label="Cookie-Einstellungen" />
-
-              <div className="mb-8 flex items-center gap-4 sm:mb-0 sm:ml-auto lg:ml-0">
-                {footerConfig?.socialLinks?.map((link, index) => (
-                  <Link key={index} href={link.url || ""} className="h-6 w-6">
-                    <Image
-                      key={index}
-                      src={link.logoIcon?.asset?.url || ""}
-                      width={100}
-                      height={100}
-                      alt={link.platformName || ""}
-                      className="h-full w-full object-contain"
-                    />
-                  </Link>
-                ))}
+                </div>
               </div>
             </div>
-          </div>
-        </footer>
-      </Container>
+          </footer>
+        </Container>
+      </div>
     </div>
   );
 };
