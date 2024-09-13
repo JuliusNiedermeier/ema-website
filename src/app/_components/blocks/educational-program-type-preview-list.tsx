62d6cf08058511fb1c7b96ef7557c4b5d7d25484
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-09-13 16:27:46 +0200

    Added new educational-program-type-preview-list compound inside educational-program-type-preview-list block

--- src/app/_components/blocks/educational-program-type-preview-list.tsx
+++ src/app/_components/blocks/educational-program-type-preview-list.tsx
@@ -1,18 +1,12 @@
 import { groq } from "next-sanity";
-import { ComponentProps, FC, Fragment } from "react";
-import { cn } from "~/app/_utils/cn";
+import { ComponentProps, FC } from "react";
 import { sanityFetch } from "~/sanity/lib/client";
 import {
   ProgramTypePreviewListProgramsQueryResult,
   ProgramTypePreviewListTypesQueryResult,
 } from "../../../../generated/sanity/types";
-import { Heading, Label, Paragraph } from "../primitives/typography";
-import { Card } from "../primitives/card";
-import Link from "next/link";
-import { InteractionBubble } from "../compounds/interaction-bubble";
-import { ChevronDown } from "lucide-react";
-import { EducationalProgramCard } from "../compounds/educational-program-card";
-import { createColorThemeStyles, ensureValidHSL } from "~/app/_utils/color-swatch";
+import { EducationalProgramTypePreviewList as ClientList } from "~/app/_components/compounds/educational-program-type-preview-list";
+import { ensureValidHSL } from "~/app/_utils/color-swatch";
 
 const programTypePreviewListTypesQuery = groq`*[_type == "educational-program-type"]{
   ...,
@@ -20,15 +14,13 @@ const programTypePreviewListTypesQuery = groq`*[_type == "educational-program-ty
 
 const programTypePreviewListProgramsQuery = groq`*[_type == "educational-program"]{
   ...,
+  coverImage { asset -> { url } },
   educationalProgramType->{ _id }
 }`;
 
-export type EducationalProgramTypePreviewListProps = ComponentProps<"div"> & {};
+export type EducationalProgramTypePreviewListProps = Omit<ComponentProps<typeof ClientList>, "programTypes"> & {};
 
-export const EducationalProgramTypePreviewList: FC<EducationalProgramTypePreviewListProps> = async ({
-  className,
-  ...restProps
-}) => {
+export const EducationalProgramTypePreviewList: FC<EducationalProgramTypePreviewListProps> = async (props) => {
   const programTypes = await sanityFetch<ProgramTypePreviewListTypesQueryResult>(programTypePreviewListTypesQuery, {
     tags: ["educational-program-type"],
   });
@@ -36,63 +28,25 @@ export const EducationalProgramTypePreviewList: FC<EducationalProgramTypePreview
     tags: ["educational-program", "educational-program-type"],
   });
 
-  const programTypesWithPrograms = programTypes.map((programType) => ({
-    ...programType,
-    introduction:
-      (programType.introduction?.length || 0) > 250
-        ? `${programType.introduction?.slice(0, 250)}...`
-        : programType.introduction,
-    programs: programs.filter((program) => program.educationalProgramType?._id === programType._id),
-  }));
-
-  return (
-    <div className={cn("flex flex-col items-center gap-16", className)} {...restProps}>
-      {programTypesWithPrograms.map((programType, index) => (
-        <Fragment key={programType._id}>
-          <div
-            className={cn(
-              "flex flex-col gap-4 rounded-3xl border p-2 lg:flex-row lg:items-stretch lg:rounded-[2rem] lg:p-4",
-              {
-                "lg:flex-row-reverse": index % 2 !== 0,
-              },
-            )}
-          >
-            <Card
-              asChild
-              className={cn(
-                "group flex flex-col justify-center transition-colors lg:flex-1 lg:items-start lg:text-left [&:not(:hover)]:bg-transparent",
-              )}
-            >
-              <Link href={`/bildungswege/${programType.slug?.current}`}>
-                <Heading className="max-w-60 text-balance lg:max-w-96">{programType.promotionalHeadline}</Heading>
-                <Paragraph className="text-balance">{programType.introduction}</Paragraph>
-                <div className="mt-4 flex items-center gap-4">
-                  <InteractionBubble animated={false} />
-                  <Label>{programType.readMoreLabel}</Label>
-                </div>
-              </Link>
-            </Card>
-            <div className={cn("flex flex-col gap-2 lg:min-w-[60%] lg:flex-row lg:gap-4")}>
-              {programType.programs.map((program) => (
-                <Link
-                  key={program._id}
-                  href={`/bildungswege/${programType.slug?.current}/${program.slug?.current}`}
-                  className="flex-1 lg:w-min"
-                >
-                  <EducationalProgramCard
-                    className="h-full pt-8"
-                    style={createColorThemeStyles(ensureValidHSL(programType.color?.hsl))}
-                    programType={programType.name || ""}
-                    name={program.name || ""}
-                    headline={program.promotionalHeadline || ""}
-                  />
-                </Link>
-              ))}
-            </div>
-          </div>
-          {index < programTypesWithPrograms.length - 1 && <ChevronDown />}
-        </Fragment>
-      ))}
-    </div>
+  const programTypesWithPrograms = programTypes.map<ComponentProps<typeof ClientList>["programTypes"][0]>(
+    (programType) => ({
+      slug: programType.slug?.current || "",
+      color: ensureValidHSL(programType.color?.hsl),
+      name: programType.name || "",
+      heading: programType.promotionalHeadline || "",
+      description: programType.introduction || "",
+      readMoreLabel: programType.readMoreLabel || "",
+      programs: programs
+        .filter((program) => program.educationalProgramType?._id === programType._id)
+        .map((program) => ({
+          slug: program.slug?.current || "",
+          name: program.name || "",
+          heading: program.promotionalHeadline || "",
+          description: program.introduction || "",
+          imageURL: program.coverImage?.asset?.url || "",
+        })),
+    }),
   );
+
+  return <ClientList programTypes={programTypesWithPrograms} {...props} />;
 };
