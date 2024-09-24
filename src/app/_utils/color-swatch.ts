466fc9f402e1c8010f633197ec665b66d08ea405
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-09-24 20:39:24 +0200

    Added foreground color to color swatch utils

--- src/app/_utils/color-swatch.ts
+++ src/app/_utils/color-swatch.ts
@@ -1,7 +1,7 @@
 import { CSSProperties } from "react";
 
 export type HSLValue = { h: number; s: number; l: number };
-export type HSLColorSwatch = { primary: HSLValue; secondary: HSLValue };
+export type HSLColorSwatch = { primary: HSLValue; secondary: HSLValue; foreground: HSLValue };
 
 export const ensureValidHSL = (hsl?: Partial<HSLValue>, scaleDecimalPercentages: boolean = true): HSLValue => {
   const factor = scaleDecimalPercentages ? 100 : 1;
@@ -12,15 +12,17 @@ export const createColorSwatch = (hsl: HSLValue): HSLColorSwatch => {
   return {
     primary: hsl,
     secondary: { ...hsl, s: hsl.s + 10, l: hsl.l + 4 },
+    foreground: { ...hsl, l: hsl.l - 40 },
   };
 };
 
 export const createHSLString = ({ h, s, l }: HSLValue) => `${h}deg ${s}% ${l}%`;
 
 export const createColorThemeStyles = (hsl: HSLValue): CSSProperties => {
-  const { primary, secondary } = createColorSwatch(hsl);
+  const { primary, secondary, foreground } = createColorSwatch(hsl);
   return {
     "--themed-primary": createHSLString(primary),
     "--themed-secondary": createHSLString(secondary),
+    "--themed-foreground": createHSLString(foreground),
   };
 };
