a0d73e1810cb8217bfb669988a43e359875436ac
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-09-26 18:55:27 +0200

    Added ComparisonTeaserCard to programs menu

--- src/app/_components/blocks/site-header/menus/programs-menu.tsx
+++ src/app/_components/blocks/site-header/menus/programs-menu.tsx
@@ -7,8 +7,9 @@ import { InteractionBubble } from "~/app/_components/compounds/interaction-bubbl
 import { groq } from "next-sanity";
 import { ProgramsMenuCheckupQueryResult } from "../../../../../../generated/sanity/types";
 import { sanityFetch } from "~/sanity/lib/client";
-import Image from "next/image";
 import { ProgramGrid } from "../../program-grid";
+import { ComparisonTeaserCard } from "../../comparison-teaser-card";
+import Link from "next/link";
 
 const programsMenuCheckupQuery = groq`*[_type == "checkup-page"][0]{
   heading,
@@ -25,28 +26,24 @@ export const OffersMenu: FC<OffersMenuProps> = async ({ className, ...restProps 
 
   return (
     <div className={cn("flex h-min flex-col-reverse gap-3 p-3 sm:items-stretch xl:flex-row", className)} {...restProps}>
-      <Card
-        className="flex w-full flex-col justify-end rounded-3xl bg-primary-900 p-2 text-neutral-900-text xl:max-w-96"
-        data-animate
-      >
-        <Image
-          src="/checkup-card.png"
-          height={500}
-          width={500}
-          alt="Checkup graphic"
-          className="overflow-hidden rounded-2xl"
-        />
-        <div className="p-6">
-          <Heading size="sm" className="mt-8">
-            {checkup?.heading}
-          </Heading>
-          <Paragraph className="flex-1xsd text-neutral-900-text-muted">{checkup?.previewText}</Paragraph>
-          <Button vairant="outline" size="sm" className="mt-8" href="/checkup">
-            <Label>{checkup?.previewReadMoreLabel}</Label>
-            <InteractionBubble />
-          </Button>
-        </div>
-      </Card>
+      <Link href="/vergleich">
+        <Card
+          className="group flex h-full w-full flex-col justify-end rounded-3xl bg-primary-900 p-2 text-neutral-900-text xl:max-w-96"
+          data-animate
+        >
+          <ComparisonTeaserCard className="flex-[10rem] bg-neutral-100/10 xl:flex-1" />
+          <div className="p-6">
+            <Heading size="sm" className="mt-8">
+              {checkup?.heading}
+            </Heading>
+            <Paragraph className="flex-1xsd text-neutral-900-text-muted">{checkup?.previewText}</Paragraph>
+            <Button vairant="outline" size="sm" className="mt-8">
+              <Label>{checkup?.previewReadMoreLabel}</Label>
+              <InteractionBubble />
+            </Button>
+          </div>
+        </Card>
+      </Link>
 
       <ProgramGrid />
     </div>
