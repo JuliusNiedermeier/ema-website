44d72535c8050289da2b9a562675e9c7286ab401
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-09-28 00:04:25 +0200

    Improved chip component

--- src/app/_components/primitives/chip.tsx
+++ src/app/_components/primitives/chip.tsx
@@ -6,7 +6,10 @@ export type ChipProps = ComponentProps<"div"> & {};
 export const Chip: FC<ChipProps> = ({ className, ...restProps }) => {
   return (
     <div
-      className={cn("text-sm w-min whitespace-nowrap rounded-full bg-primary-100 px-4 py-1", className)}
+      className={cn(
+        "text-sm flex w-min items-center gap-4 whitespace-nowrap rounded-full bg-primary-100 px-4 py-1",
+        className,
+      )}
       {...restProps}
     />
   );
