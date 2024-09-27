25f2de29b88d5522e5a36addb080be75510760b0
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-09-28 00:05:17 +0200

    Added chip primitive in requirement-list

--- src/app/_components/compounds/requirement-list.tsx
+++ src/app/_components/compounds/requirement-list.tsx
@@ -2,6 +2,7 @@ import { ComponentProps, FC, Fragment } from "react";
 import { cn } from "~/app/_utils/cn";
 import { Label } from "../primitives/typography";
 import { CheckIcon } from "lucide-react";
+import { Chip } from "../primitives/chip";
 
 export type RequirementListProps = ComponentProps<"div"> & {
   groups: string[][];
@@ -15,12 +16,12 @@ export const RequirementList: FC<RequirementListProps> = ({ className, groups, s
         <Fragment key={index}>
           <div className="flex max-w-[40rem] flex-col gap-2">
             {group.map((requirement, index) => (
-              <div key={index} className="flex items-center gap-4 rounded-full bg-themed-primary p-2 pr-8">
+              <Chip key={index} className="bg-themed-primary p-2 pr-8 w-full">
                 <div className="rounded-full bg-primary-900 p-2 text-neutral-100">
                   <CheckIcon />
                 </div>
                 <Label>{requirement}</Label>
-              </div>
+              </Chip>
             ))}
           </div>
           {index < array.length - 1 && (
