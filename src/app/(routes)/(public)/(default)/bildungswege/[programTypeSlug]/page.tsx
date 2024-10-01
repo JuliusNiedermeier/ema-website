d0f52fd5a8e87cd290cc7db876c412d093684c87
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-10-01 13:09:45 +0200

    Improved certificate and followup section on program type page. Partially filled with mock data

--- src/app/(routes)/(public)/(default)/bildungswege/[programTypeSlug]/page.tsx
+++ src/app/(routes)/(public)/(default)/bildungswege/[programTypeSlug]/page.tsx
@@ -25,13 +25,12 @@ import {
 import { InteractionBubble } from "~/app/_components/compounds/interaction-bubble";
 import { IconChip } from "~/app/_components/primitives/icon-chip";
 import { ArrowDown, ArrowRightIcon, BadgeIcon, CheckIcon } from "lucide-react";
+import { Chip } from "~/app/_components/primitives/chip";
+import { mapRange } from "~/app/_utils/map-range";
 import { Card } from "~/app/_components/primitives/card";
 import Image from "next/image";
 import { GradientStrokeIcon } from "~/app/_components/primitives/gradient-stroke-icon";
 import { GradientStroke } from "~/app/_components/primitives/gradient-stroke";
-import { IconChip } from "~/app/_components/primitives/icon-chip";
-import { BadgeIcon, CheckIcon, PlusIcon } from "lucide-react";
-import { Chip } from "~/app/_components/primitives/chip";
 
 // const programTypePageSlugsQuery = groq`*[_type == "educational-program-type"]{ slug }`;
 
@@ -43,7 +42,16 @@ const programTypePageQuery = groq`*[_type == "educational-program-type" && slug.
       ...,
       image { asset -> { url } }
     }
-  }
+  },
+  followUpProgramTypes {
+    ...,
+    programTypes[] -> {
+      slug,
+      name,
+      promotionalHeadline,
+      color
+    }
+  },
 }`;
 
 const programTypePageProgramsQuery = groq`*[_type == "educational-program" && educationalProgramType -> slug.current == $programTypeSlug]`;
@@ -93,71 +101,84 @@ const EducationalProgramTypePage: FC<Props> = async ({ params: { programTypeSlug
         </Container>
       </div>
 
-      <Section className="bg-neutral-400">
-        <div className="py-24">
-          <Container className="" width="narrow">
-            <GradientStrokeIcon>
-              <GradientStroke />
-              <IconChip>
-                <BadgeIcon />
-              </IconChip>
-            </GradientStrokeIcon>
-            <div className="mt-8 text-center">
-              <Heading>Du wirst Staatlich Anerkannter Sozialassistent</Heading>
-              <Paragraph className="mt-8">
+      <Section className="overflow-hidden bg-primary-900">
+        <Container className="flex flex-col pb-8 xl:flex-row xl:items-end xl:pb-0">
+          <div className="flex-[0_0_50%] pb-16 pt-32 xl:pb-48 xl:pr-24">
+            <IconChip className="bg-primary-100x bg-themed-primary text-primary-100-text">
+              <BadgeIcon />
+            </IconChip>
+            <div className="mt-16 max-w-[40rem] text-balance">
+              <Heading className="text-neutral-900-text">Du wirst Staatlich Anerkannter Sozialassistent</Heading>
+              <Paragraph className="text-neutral-900-text-muted">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere perspiciatis quae commodi cum adipisci
                 cupiditate animi praesentium veritatis! Dolorem officia nemo distinctio corporis. Quae consectetur
                 pariatur recusandae commodi natus aliquid!
               </Paragraph>
             </div>
-          </Container>
-
-          <Container className="mt-12 flex flex-wrap justify-center gap-4">
-            {Array.from(new Array(10)).map((_, index) => (
-              <Chip key={index} className="bg-neutral-100 p-2 pr-6">
-                <div className="rounded-full bg-primary-900 p-2 text-neutral-100">
-                  <CheckIcon />
-                </div>
-                <Label>Studium</Label>
-              </Chip>
-            ))}
-          </Container>
-        </div>
-      </Section>
-
-      <Section className="bg-neutral-300">
-        <IconChip className="mx-auto -translate-y-1/2">
-          <PlusIcon />
-        </IconChip>
-        <div className="pb-4 pt-24 sm:py-24">
-          <Container width="narrow">
-            <div className="text-center">
-              <Heading>Qualifiziere dich für das Fachabitur!</Heading>
-              <Paragraph>Du kannst im Anschluss dein Fachabi bei uns machen.</Paragraph>
+            <div className="relative mt-12 flex flex-wrap gap-4">
+              <div className="absolute bottom-0 top-0 z-10 w-full bg-gradient-to-b from-transparent to-primary-900/90" />
+              {[
+                "Bäcker",
+                "Sportler",
+                "Produkttester",
+                "Mailschreiber",
+                "Berufsbauer",
+                "Elektroniker",
+                "Arzt",
+                "Beinträchtigter Maurer",
+              ].map((label, index, array) => (
+                <Chip
+                  key={index}
+                  className="flex-1 bg-neutral-100 p-2 pr-6"
+                  style={{ opacity: mapRange(index, [0, array.length], [1, 0]) }}
+                >
+                  <div className="rounded-full bg-primary-900 p-2 text-neutral-100">
+                    <CheckIcon />
+                  </div>
+                  <Label>{label}</Label>
+                </Chip>
+              ))}
             </div>
-          </Container>
-          <Container>
-            <LinkCardCollection className="mt-16 justify-center">
-              {Array.from(new Array(1))
-                .fill(null)
-                .map((followUpProgram, index) => (
+          </div>
+
+          <div className="relative mt-auto flex-[0_0_50%] flex-col p-2 sm:p-8 xl:overflow-visible xl:pb-24 xl:pr-0">
+            <div className="absolute left-0 top-0 h-full w-full rounded-3xl bg-neutral-100/10 xl:h-[calc(100%+4rem)] xl:w-[50vw] xl:rounded-none xl:rounded-tl-3xl" />
+            <div className="relative">
+              <div className="sm: max-w-[30rem] p-6 sm:p-0">
+                <IconChip className="bg-themed-primary text-primary-100-text">
+                  <ArrowRightIcon />
+                </IconChip>
+                <Heading size="sm" className="mt-16 text-neutral-900-text">
+                  Ermögliche dir dein Studium
+                </Heading>
+                <Paragraph className="text-neutral-900-text-muted">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere perspiciatis quae commodi cum adipisci
+                  cupiditate animi praesentium veritatis!
+                </Paragraph>
+              </div>
+              <LinkCardCollection className="mt-2 sm:mt-12">
+                {programType?.followUpProgramTypes?.programTypes?.map((followUpProgramType, index) => (
                   <Link
                     key={index}
-                    href={`/bildungswege/${"-"}/${"-"}`}
-                    className="!min-w-[min(24rem,100%)] !flex-grow-0"
+                    href={`/bildungswege/${followUpProgramType.slug?.current}`}
+                    className="!min-w-[min(24rem,100%)]"
                   >
-                    <LinkCard className="border-none bg-themed-primary p-4 hover:bg-themed-secondary">
+                    <LinkCard
+                      style={createColorThemeStyles(ensureValidHSL(followUpProgramType.color?.hsl))}
+                      className="border-none bg-themed-primary p-4 hover:bg-themed-secondary"
+                    >
                       <InteractionBubble animated={false} />
                       <LinkCardContent>
-                        <LinkCardTitle>{"Bildungsweg"}</LinkCardTitle>
-                        <LinkCardSubtitle>{"Dies ist ein platzhalter text"}</LinkCardSubtitle>
+                        <LinkCardTitle>{followUpProgramType.name}</LinkCardTitle>
+                        <LinkCardSubtitle>{followUpProgramType.promotionalHeadline}</LinkCardSubtitle>
                       </LinkCardContent>
                     </LinkCard>
                   </Link>
                 ))}
-            </LinkCardCollection>
-          </Container>
-        </div>
+              </LinkCardCollection>
+            </div>
+          </div>
+        </Container>
       </Section>
 
       <Section className="bg-neutral-200">
@@ -183,20 +204,20 @@ const EducationalProgramTypePage: FC<Props> = async ({ params: { programTypeSlug
                   <Card className="group rounded-3xl border-none bg-themed-primary p-2 transition-colors hover:bg-themed-secondary">
                     <div className="relative aspect-video overflow-hidden rounded-2xl">
                       <Image src="/campus.png" alt="" fill />
-          </div>
+                    </div>
                     <div className="flex items-center gap-4 p-6">
                       <LinkCardContent className="">
-                    <LinkCardLabel>{programType.name}</LinkCardLabel>
-                    <LinkCardTitle>{program.name}</LinkCardTitle>
-                    <LinkCardSubtitle>{program.promotionalHeadline}</LinkCardSubtitle>
-                  </LinkCardContent>
+                        <LinkCardLabel>{programType.name}</LinkCardLabel>
+                        <LinkCardTitle>{program.name}</LinkCardTitle>
+                        <LinkCardSubtitle>{program.promotionalHeadline}</LinkCardSubtitle>
+                      </LinkCardContent>
                       <InteractionBubble animated={false} />
                     </div>
                   </Card>
-              </Link>
-            ))}
-          </LinkCardCollection>
-        </Container>
+                </Link>
+              ))}
+            </LinkCardCollection>
+          </Container>
         </div>
       </Section>
 
