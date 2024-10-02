dbc92a3facaeeaca32293f588f1141445d6592f2
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-10-02 15:21:13 +0200

    Removed metadata from /studio layout. Meta data is controlled by sanity studio anyways

--- src/app/(routes)/(private)/studio/layout.tsx
+++ src/app/(routes)/(private)/studio/layout.tsx
@@ -1,10 +1,5 @@
 import { FC, PropsWithChildren } from "react";
 
-export const metadata = {
-  title: "EMA Content Studio",
-  description: "",
-};
-
 const StudioLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <html lang="de">
