d975e7d60f8cab2dd266475f6c35f0ef8868fa5f
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-10-04 08:37:30 +0200

    Improved portable-image cms type

--- src/sanity/schema/partials/portable-image.ts
+++ src/sanity/schema/partials/portable-image.ts
@@ -3,12 +3,11 @@ import { defineArrayMember } from "sanity";
 export const portableImageType = defineArrayMember({
   name: "portableImage",
   type: "image",
-  options: { hotspot: true },
   fields: [
     {
       name: "alt",
       type: "string",
-      title: "Alternative Text",
+      title: "Alternativtext",
     },
   ],
 });
