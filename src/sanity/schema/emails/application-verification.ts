22e2fd08135cc7d19abe1ef8d0ed2904ba43532c
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-10-13 21:29:57 +0200

    Fixed application-verification file extension (.tsx -> .ts)

--- src/sanity/schema/emails/application-verification.ts
+++ src/sanity/schema/emails/application-verification.ts
@@ -1,6 +1,7 @@
 import { defineField, defineType } from "sanity";
 import { SchemaTypeDef } from "..";
 import { BadgeCheckIcon } from "lucide-react";
+import { createStringValidation } from "~/sanity/lib/validations";
 
 export const applicationVerificationEmail: SchemaTypeDef = {
   type: "email",
@@ -15,27 +16,36 @@ export const applicationVerificationEmail: SchemaTypeDef = {
         title: "Überschrift",
         description: "Füge {name} in den Text ein, um dort den Namen des Bewerbers anzuzeigen.",
         type: "string",
+        validation: createStringValidation("heading"),
       }),
+
       defineField({
         name: "body",
         title: "Textinhalt",
         description: "Füge {name} in den Text ein, um dort den Namen des Bewerbers anzuzeigen.",
         type: "text",
+        validation: createStringValidation("description"),
       }),
+
       defineField({
         name: "verifyButtonLabel",
         title: "Text auf dem Bestätigungsbutton",
         type: "string",
+        validation: createStringValidation("label"),
       }),
+
       defineField({
         name: "regards",
         title: "Grußfloskel",
         type: "string",
+        validation: createStringValidation([10, 100]),
       }),
+
       defineField({
         name: "senderName",
         title: "Name des Teams",
         type: "string",
+        validation: createStringValidation("name"),
       }),
     ],
     preview: {
