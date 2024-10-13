7eae1cbbd53f645f67cd73c45ce5fa6c83d13d5f
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-10-14 00:18:49 +0200

    Increased validation range for headings to max 60 chars

--- src/sanity/lib/validations.ts
+++ src/sanity/lib/validations.ts
@@ -7,7 +7,7 @@ type NamedStringSizeBounds = keyof typeof stringSizeBoundsMap;
 
 const stringSizeBoundsMap = {
   label: [2, 30],
-  heading: [5, 50],
+  heading: [5, 60],
   description: [100, 300],
   name: [3, 30],
 } satisfies Record<string, StringSizeBounds>;
