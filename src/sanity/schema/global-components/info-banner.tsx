a6e4c7ceab85bf584edf296cfb87767989dbd3ed
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-10-19 02:19:20 +0200

    Added usage intructions for content editors to info-banner cms type

--- src/sanity/schema/global-components/info-banner.tsx
+++ src/sanity/schema/global-components/info-banner.tsx
@@ -2,6 +2,7 @@ import { defineField, defineType } from "sanity";
 import { SchemaTypeDef } from "..";
 import { FlagIcon } from "lucide-react";
 import { createStringValidation, getSizeString } from "~/sanity/lib/validations";
+import { Text } from "@sanity/ui";
 
 export const infoBannerConfigType: SchemaTypeDef = {
   type: "global-component",
@@ -11,6 +12,20 @@ export const infoBannerConfigType: SchemaTypeDef = {
     type: "document",
     icon: FlagIcon,
     fields: [
+      defineField({
+        name: "info",
+        type: "boolean",
+        components: {
+          field: () => (
+            <Text>
+              Die Sichtbarkeit des Infobanners wird durch das veröffentlichen und das aufheben der Veröffentlichung
+              gesteuert. Um den Infobanner zu aktivieren klickst du unten Rechts auf auf "Publish". Um ihn zu
+              deaktivieren, klickst du unten rechts auf "..." {"->"} "Unpublish". Nach jedem veröffentlichen wird der
+              Banner wieder allen Nutzern angezeigt. Auch denen die ihn zuvor ausgeblendet haben.
+            </Text>
+          ),
+        },
+      }),
       defineField({
         name: "content",
         title: "Info-Text",
