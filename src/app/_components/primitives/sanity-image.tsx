6cc903262127125a282454015fd7c79e3a9a967e
JuliusNiedermeier <julius.niedermeier@outlook.de>
2024-10-22 18:13:00 +0200

    Moved sanity image url util into a dedicated SanityImage component

---
+++ src/app/_components/primitives/sanity-image.tsx
@@ -0,0 +1,25 @@
+import Image, { ImageProps } from "next/image";
+import { FC } from "react";
+import createImageUrlBuilder from "@sanity/image-url";
+import { env } from "~/env";
+import { DefaultImage } from "../../../../generated/sanity/types";
+import { cn } from "~/app/_utils/cn";
+
+const imageBuilder = createImageUrlBuilder({
+  projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: env.NEXT_PUBLIC_SANITY_DATASET,
+});
+
+type SanityImageProps = Omit<ImageProps, "src" | "alt"> & {
+  image?: Omit<DefaultImage, "_type"> | null;
+};
+
+export const SanityImage: FC<SanityImageProps> = ({ image, className, ...restProps }) => {
+  if (!image?.asset) return null;
+
+  const baseImage = imageBuilder.image(image.asset._ref).auto("format").fit("crop").quality(80);
+
+  return (
+    <Image src={baseImage.url() || ""} alt={image.alt || ""} className={cn("object-cover", className)} {...restProps} />
+  );
+};
